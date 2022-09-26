4e0d4623ae1c1ffb2d8d852c94b843005bcd65f1
Spotky1004 <spotky1004@gmail.com>
2022-09-26 23:11:04 +0900

    Update Baek 13448

--- Baek/Baek13448.js
+++ Baek/Baek13448.js
@@ -3,11 +3,10 @@ const input = (
   !isDev
     ? require("fs").readFileSync("/dev/stdin").toString()
     :
-`3 75
-250 500 1000
-2 4 8
-99 75 99
-`
+`1 74
+502
+2
+47`
 )
   .trim()
   .split("\n").map(line => line.split(" ").map(Number));
@@ -22,20 +21,31 @@ const P = input.shift();
 const R = input.shift();
 /** @type {number[]} */
 const problems = M.map((_, i) => [M[i], P[i], R[i]])
-  .sort(([am, ap, ar], [bm, bp, br]) => (bm-br*bp) - (am-ar*ap));
+  .sort(([am, ap, ar], [bm, bp, br]) => (Math.max(0, bm-br*bp) + Math.max(0, am-(ar+br)*ap)) - (Math.max(0, am-ar*ap) + Math.max(0, bm-(br+ar)*bp)));
 
-/** @type {number[]} */
-const dp = [0];
-for (let i = 0; i < N; i++) {
-  const [m, p, r] = problems[i];
-  if (r > T) continue;
-  for (let j = Math.min(dp.length + 1, T - r); j >= 0; j--) {
-    const scoreGot = m-(r+j)*p;
-    if (scoreGot <= 0) break;
-    const dpValue = dp[j];
-    if (typeof dpValue === "undefined") continue;
-    dp[r+j] = Math.max(dpValue + scoreGot, dp[r+j] ?? 0);
+/** @type {[number, number[]][]} */
+const dp = [[0, []]];
+for (let l = 0; l < N; l++) {
+  for (let i = 0; i < N; i++) {
+    const [m, p, r] = problems[i];
+    if (r > T) continue;
+    for (let j = Math.min(dp.length - 1, T - r); j >= 0; j--) {
+      const scoreGot = m-(r+j)*p;
+      if (scoreGot <= 0) break;
+      if (typeof dp[j] === "undefined") continue;
+      const [dpValue, used] = dp[j];
+      const prevDp = (dp[r+j] ?? [0, []])[0];
+      if (
+        used.includes(i) ||
+        (dp[r+j] ?? [0])[0] > dpValue + scoreGot ||
+        (
+          (dp[r+j] ?? [0])[0] === dpValue + scoreGot &&
+          prevDp.length < used.length + 1
+        )
+      ) continue;
+      dp[r+j] = [dpValue + scoreGot, [...used, i]];
+    }
   }
 }
 
-console.log(dp, Math.max(0, ...dp.filter(v => v)));
+console.log(Math.max(0, ...dp.map(v => v[0]).filter(v => v)));

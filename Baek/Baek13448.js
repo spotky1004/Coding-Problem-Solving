2a8c67b7c6151c90a1226f3be19d396f60cc724f
Spotky1004 <spotky1004@gmail.com>
2022-09-26 16:40:30 +0900

    Update Baek 13448

--- Baek/Baek13448.js
+++ Baek/Baek13448.js
@@ -3,10 +3,10 @@ const input = (
   !isDev
     ? require("fs").readFileSync("/dev/stdin").toString()
     :
-`3 30
-100 100 100000
-1 1 100
-15 15 30
+`3 75
+250 500 1000
+2 4 8
+99 75 99
 `
 )
   .trim()
@@ -25,20 +25,17 @@ const problems = M.map((_, i) => [M[i], P[i], R[i]])
   .sort(([am, ap, ar], [bm, bp, br]) => (bm-br*bp) - (am-ar*ap));
 
 /** @type {number[]} */
-const dp = [];
+const dp = [0];
 for (let i = 0; i < N; i++) {
   const [m, p, r] = problems[i];
   if (r > T) continue;
-  for (let j = Math.min(dp.length - 1, T - r); j >= 0; j--) {
+  for (let j = Math.min(dp.length + 1, T - r); j >= 0; j--) {
     const scoreGot = m-(r+j)*p;
     if (scoreGot <= 0) break;
     const dpValue = dp[j];
     if (typeof dpValue === "undefined") continue;
     dp[r+j] = Math.max(dpValue + scoreGot, dp[r+j] ?? 0);
   }
-  const scoreGot = m-r*p;
-  if (scoreGot <= 0) continue;
-  dp[r] = Math.max(m-r*p, dp[r] ?? 0);
 }
 
-console.log(Math.max(0, ...dp.filter(v => v)));
+console.log(dp, Math.max(0, ...dp.filter(v => v)));

fe7f9f65ad09e5b9e298646db7c1bbad53f90e45
Spotky1004 <spotky1004@gmail.com>
2022-10-06 10:23:09 +0900

    Update dijkstra module

--- Baek/module/dijkstra.js
+++ Baek/module/dijkstra.js
@@ -7,12 +7,18 @@ for (const [from, to, cost] of rawLines) {
   nodeFrom.set(to, Math.min(nodeFrom.get(to) ?? Infinity, cost));
 }
 
-function dijkstra() {
-  const costs = Array(V + 1).fill(Infinity);
-  costs[K] = 0;
+/**
+ * @param {number} nodeCount 
+ * @param {number} startNode 
+ * @param {Map<number, number>[]} nodesMap 
+ * @returns {number[]}
+ */
+function dijkstra(nodeCount, startNode, nodesMap) {
+  const costs = Array(nodeCount + 1).fill(Infinity);
+  costs[startNode] = 0;
   /** @type {Heap<[node: number, cost: number]>} */
   const heap = new Heap((a, b) => a[1] - b[1]);
-  heap.insert([K, 0]);
+  heap.insert([startNode, 0]);
   while (heap.size > 0) {
     /** @type {number} */
     const [nodeNr, cost] = heap.delete();

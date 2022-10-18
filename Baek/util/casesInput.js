d747ce9964d6e885b0e3c62033db8e82cfab2ce8
Spotky1004 <spotky1004@gmail.com>
2022-10-18 09:13:14 +0900

    Create casesInput util

---
+++ Baek/util/casesInput.js
@@ -0,0 +1,16 @@
+const isDev = process.platform !== "linux";
+const input = (
+  !isDev
+    ? require("fs").readFileSync("/dev/stdin").toString()
+    :
+``
+)
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+
+let i = 0;
+while (i < input.length) {
+  const t = input[i];
+  i++;
+}

b78a46d519e6b91f6667066def8c7ab48b3e50f4
Spotky1004 <spotky1004@gmail.com>
2022-10-18 16:25:02 +0900

    Solved Baek 9735

---
+++ Baek/solved/Baek9735.js
@@ -0,0 +1,103 @@
+const isDev = process.platform !== "linux";
+const m = 100000000000000000000000000000000000n;
+const [, ...questions] = (
+  !isDev
+    ? require("fs").readFileSync("/dev/stdin").toString()
+    :
+`2
+2 -7 7 -2
+2 0 0 0
+1 -2 0 0
+-1 5 2 0
+`
+)
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(t => BigInt(t)));
+
+function sqrt(a) {
+  const _m = m**2n;
+  const fixedNum = a * _m**2n;
+  if (fixedNum < 0) return 0n;
+  const nIterCount = 4000;
+  const iterCount = BigInt(nIterCount);
+
+  let cur = 0n;
+  let jump = 2n ** iterCount;
+  while (true) {
+    const curCube = cur ** 2n;
+    const curAccCube = (cur + 1n) ** 2n;
+    if (curCube <= fixedNum && fixedNum <= curAccCube) {
+      break;
+    }
+    if (curCube > fixedNum) {
+      cur -= jump;
+    } else {
+      cur += jump;
+    }
+    jump /= 2n;
+  }
+  return cur / _m;
+}
+function sign(a) {
+  return a > 0n ? 1n : a === 0n ? 0n : -1n
+}
+function calc(a, b, c, d, x) {
+  return a*x**3n/m**2n + b*x**2n/m + c*x + d*m;
+}
+function search(type, from, to, a, b, c, d) {
+  if (type === 0n) return null;
+  let left = from;
+  let right = to;
+  let cur = (left + right) / 2n;
+  while (-1n > left - right || left - right > 1n) {
+    const v = calc(a, b, c, d, cur);
+    if (type === 1n) {
+      if (v <= 0n) {
+        left = cur;
+      } else {
+        right = cur;
+      }
+    } else {
+      if (v >= 0n) {
+        left = cur;
+      } else {
+        right = cur;
+      }
+    }
+    cur = (left + right) / 2n;
+  }
+  const maxDiff = sqrt(m);
+  if (
+    calc(a, b, c, d, cur) > maxDiff ||
+    calc(a, b, c, d, cur) < -maxDiff
+  ) return null;
+  return cur;
+}
+
+for (const [a, b, c, d] of questions) {
+  const [i1, i2] = [
+    (-2n*m*b - sqrt(m**2n*(4n*b**2n - 12n*a*c))) / (6n*a),
+    (-2n*m*b + sqrt(m**2n*(4n*b**2n - 12n*a*c))) / (6n*a)
+  ].sort((a, b) => Number(a - b));
+  const type = sign(calc(a, b, c, d, i1) - calc(a, b, c, d, i1 - m/2n));
+  const ranges = [
+    -1_000_000n * m,
+    i1,
+    i2,
+    1_000_000n * m
+  ];
+  const out = [];
+  for (let i = 1; i < ranges.length; i++) {
+    if (ranges[i - 1] - ranges[i] === 0n) continue;
+
+    const ans = search(
+      type * (-1n)**(BigInt(i) + 1n),
+      ranges[i - 1], ranges[i],
+      a, b, c, d
+    );
+    if (ans === null) continue;
+    out.push(Number(ans*100000000n/m)/1e8);
+  }
+  console.log([...new Set(out.sort((a, b) => a - b).map(v => v.toFixed(5)))].join(" "));
+}

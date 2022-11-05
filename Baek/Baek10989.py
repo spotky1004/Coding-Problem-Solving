570495abf3ba488771c8dd4d7fddf562031b41ac
Spotky1004 <spotky1004@gmail.com>
2022-11-05 14:30:47 +0900

    Remove some scripts

--- Baek/Baek10989.py
+++
@@ -1,9 +0,0 @@
-acc = [0] * 10000
-for _ in range(0, int(input())):
-  acc[int(input()) - 1] += 1
-
-out = ""
-for i in range(1, 10000):
-  out += (str(i) + "\n") * acc[i - 1]
-
-print(out.rstrip())

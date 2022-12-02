b1f95a68467013f177eebd8d1e115202a22ac9e3
Spotky1004 <spotky1004@gmail.com>
2022-12-02 17:44:04 +0900

    Create strCompare module

---
+++ Baek/module/strCompare.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} a 
+ * @param {string} b 
+*/
+function strCompare(a, b) {
+  const len = Math.min(a.length, b.length);
+  for (let i = 0; i < len; i++) {
+    if (a[i] === b[i]) continue;
+    return a.charCodeAt(i) - b.charCodeAt(i);
+  }
+  return a.length - b.length;
+}

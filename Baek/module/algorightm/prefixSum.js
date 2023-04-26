5764f3d067852fb0b363177dccfb2e6ebbca167f
Spotky1004 <spotky1004@gmail.com>
2023-04-26 21:10:05 +0900

    Fix prefixSum module

--- Baek/module/algorightm/prefixSum.js
+++ Baek/module/algorightm/prefixSum.js
@@ -5,7 +5,7 @@ function prefixSum(arr) {
   if (arr.length === 0) return [];
 
   const sumArr = [arr[0]];
-  for (let i = 1; i <= arr.length; i++) {
+  for (let i = 1; i < arr.length; i++) {
     sumArr[i] = sumArr[i - 1] + arr[i];
   }
   return sumArr;

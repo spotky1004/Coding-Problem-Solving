f665897b5695e69c8036ee9e508450fb4e364e50
Spotky1004 <spotky1004@gmail.com>
2023-04-27 21:22:52 +0900

    Add combination module

---
+++ Baek/module/numberTheory/combination.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} n 
+ * @param {number} r 
+ * @param {number} p
+ * @param {number[]} factroials
+ */
+function combination(n, r, p, factroials) {
+  return factroials[n] * divAndPow(factroials[n - r] * factroials[r], p - 2, p) % p;
+}

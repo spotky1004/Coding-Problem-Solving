f1675af55ed77caed54b6e084c7980e2b541f801
Spotky1004 <spotky1004@gmail.com>
2023-05-07 01:56:52 +0900

    Update codeforces input

--- CodeForces/input.js
+++ CodeForces/input.js
@@ -4,3 +4,15 @@ const [, ...cases] =
   .trim()
   .split("\n")
   .map(line => line.split(" ").map(Number));
+
+const out = [];
+
+let i = 0;
+while (i < cases.length) {
+  const n = cases[i][0];
+  i++;
+
+  out.push();
+}
+
+console.log(out.join("\n"));

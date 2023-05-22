574bb2935928829211288c1d9c6246e37603e153
Spotky1004 <spotky1004@gmail.com>
2023-05-22 14:18:57 +0900

    Update Baek28068 to AC code

--- Baek/solved/Baek28068.js
+++ Baek/solved/Baek28068.js
@@ -3,19 +3,41 @@ const [[N], ...books] = (
   !isDev
     ? require("fs").readFileSync("/dev/stdin").toString()
     :
-`3
-0 11
-8 7
-10 6`
-// `4
-// 0 10
-// 10 7
-// 7 2
-// 2 0`
+`4
+0 10
+10 7
+7 2
+2 0`
 )
   .trim()
   .split("\n")
   .map(line => line.split(" ").map(Number));
+
+function tryRead() {
+  let happy = 0;
+  let bookLeft = N;
+  for (let i = 0; i < N; i++) {
+    const [ai, bi] = books[i];
+    if (ai > happy) break;
+    happy += bi - ai;
+    bookLeft--;
+  }
+  return bookLeft === 0;
+}
+
+let possible = false;
+
+books.sort(([a0, a1], [b0, b1]) => {
+  if (a0 === 0) return -1;
+  if (b0 === 0) return 1;
+  const aDif = a1 - a0;
+  const bDif = b1 - b0;
+  if (aDif >= 0 && bDif >= 0) return a0 - b0;
+  if (aDif >= 0) return -1;
+  if (bDif >= 0) return 1;
+  return b0 - a0;
+});
+if (tryRead()) possible = true;
 
 books.sort(([a0, a1], [b0, b1]) => {
   if (a0 === 0) return -1;
@@ -27,17 +49,6 @@ books.sort(([a0, a1], [b0, b1]) => {
   if (bDif >= 0) return 1;
   return bDif - aDif;
 });
+if (tryRead()) possible = true;
 
-
-let happy = 0;
-let possible = true;
-for (const [ai, bi] of books.filter(([ai, bi]) => bi - ai >= 0)) {
-  if (ai > happy) {
-    possible = false;
-    break;
-  }
-  happy += bi - ai;
-}
-
-
-console.log(possible ? 0 : 1);
+console.log(possible ? 1 : 0);

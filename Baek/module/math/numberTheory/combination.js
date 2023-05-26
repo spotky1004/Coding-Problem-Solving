962ac1e4c543c832b59c823f8c513d2ce98b8719
Spotky1004 <spotky1004@gmail.com>
2023-05-27 05:26:47 +0900

    Update combination module

--- Baek/module/math/numberTheory/combination.js
+++ Baek/module/math/numberTheory/combination.js
@@ -3,7 +3,7 @@
  * @param {number} r 
  * @param {number} p
  * @param {number[]} factroials
- */
+*/
 function combination(n, r, p, factroials) {
   return factroials[n] * divAndPow(factroials[n - r] * factroials[r], p - 2, p) % p;
 }

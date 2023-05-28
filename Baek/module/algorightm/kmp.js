bad1181c22ef2f766b92dfbd860c08938147c00a
Spotky1004 <spotky1004@gmail.com>
2023-05-28 13:13:06 +0900

    Add kmp module

---
+++ Baek/module/algorightm/kmp.js
@@ -0,0 +1,50 @@
+/**
+ * @param {string} str 
+*/
+function makeLps(str) {
+  const lps = Array(str.length);
+  lps[0] = 0;
+
+  let i = 1;
+  let j = 0;
+  while (i < str.length) {
+    if (str[i] === str[j]) {
+      lps[i] = ++j;
+      i++;
+    } else {
+      if (j !== 0) j = lps[j - 1];
+      else {
+        lps[i] = 0;
+        i++;
+      }
+    }
+  }
+
+  return lps;
+}
+
+/**
+ * @param {string} str 
+ * @param {string} pattern 
+*/
+function kmp(str, pattern) {
+  const lps = makeLps(pattern);
+  const matchIdxes = [];
+
+  let i = 0;
+  let j = 0;
+  while (i < str.length) {
+    if (str[i] === pattern[j]) {
+      i++;
+      j++;
+    }
+    if (j === pattern.length) {
+      matchIdxes.push(i - j);
+      j = lps[j - 1];
+    } else if (i < str.length && str[i] !== pattern[j]) {
+      if (j === 0) i++;
+      j = lps[j - 1] ?? 0;
+    }
+  }
+  return matchIdxes;
+}

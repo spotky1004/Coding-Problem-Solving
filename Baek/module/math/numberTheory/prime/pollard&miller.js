f17f88dcef9e6df49e6f57bec81866c2ddc76de4
Spotky1004 <spotky1004@gmail.com>
2023-06-09 01:32:38 +0900

    Add pollard&miller module

---
+++ Baek/module/math/numberTheory/prime/pollard&miller.js
@@ -0,0 +1,108 @@
+/**
+ * @param {bigint} a 
+ * @param {bigint} b 
+ * @returns {bigint} 
+*/
+function gcd(a, b) {
+  if (b > a) [a, b] = [a, b];
+  if (b === 0n) return a;
+  return gcd(b, a % b);
+}
+/**
+ * @param {bigint} a 
+ * @param {bigint} b 
+ * @param {bigint} p
+*/
+function divAndPow(a, b, p) {
+  let out = 1n;
+  let curMul = a;
+  const loopCount = BigInt(Math.ceil(Math.log2(Number(b))) + 1);
+  for (let i = 0n; i < loopCount; i++) {
+    if (b & 1n << i) {
+      out = out*curMul % p;
+    }
+    curMul = curMul**2n % p;
+  }
+  return out;
+}
+
+/**
+ * @param {bigint} n 
+ * @param {bigint} c 
+ * @returns {bigint[]} 
+ */
+function pollardRho(n, c = 1n) {
+  n = BigInt(n);
+  if (n === 1n || isPrime(n)) return n;
+  if (n % 2n === 0n) return 2n;
+
+  function g(x) {
+    return (x * x + c) % n;
+  }
+  
+  let a = 2n;
+  let b = a;
+  while (true) {
+    a = g(a);
+    b = g(g(b));
+    if (a === b) return pollardRho(n, c + 1n);
+    const dif = a - b;
+    const d = gcd(n, dif > 0 ? dif : -dif);
+    if (d === 1n) continue;
+    return d;
+  }
+}
+
+const millerRabinPrimes = [
+  2n, 3n, 5n, 7n, 11n, 13n, 17n, 19n, 23n, 29n,
+  31n, 37n, 41n, 43n, 47n, 53n, 59n, 61n, 67n, 71n
+];
+/**
+ * @param {bigint} n 
+ * @returns {boolean} 
+*/
+function isPrime(n) {
+  if (n === 2n) return true;
+  if (n < 2n) return false;
+
+  n = BigInt(n);
+  d = n - 1n;
+
+  let r = 0n;
+  while ((d & 1n) === 0n) {
+    r++;
+    d /= 2n;
+  }
+
+  l: for (const p of millerRabinPrimes) {
+    if (n === p) return true;
+    if (divAndPow(p, d, n) === 1n) continue;
+    for (let i = 0n; i < r; i++) {
+      if (divAndPow(p, 2n**i * d, n) === n - 1n) continue l;
+    }
+    return false;
+  }
+  return true;
+}
+
+/**
+ * @param {bigint} x 
+ */
+function primeFactorization(x) {
+  if (x <= 3n) return [x];
+  const toFactorization = [x];
+  const factors = [];
+  for (const n of toFactorization) {
+    if (isPrime(n)) {
+      factors.push(n);
+      continue;
+    }
+    const p = pollardRho(n);
+    const q = n / p;
+    if (isPrime(p)) factors.push(p);
+    else if (p !== 1n) toFactorization.push(p);
+    if (isPrime(q)) factors.push(q);
+    else if (q !== 1n) toFactorization.push(q);
+  }
+  return factors.sort((a, b) => Number(a - b));
+}

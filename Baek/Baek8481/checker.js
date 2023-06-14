ae5cf5d4bb70dd9fa58746f3a5954c175ec5b6a3
Spotky1004 <spotky1004@gmail.com>
2023-06-14 20:31:02 +0900

    Solved Baek8481: 9

--- Baek/Baek8481/checker.js
+++ Baek/Baek8481/checker.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const toCheck = 8;
+const toCheck = 9;
 
 const outFile = fs.readFileSync("./out.txt", "utf8");
 const ansFile = fs.readFileSync(`./Baek/Baek8481/genzaw/gen${toCheck}.out`, "utf8");

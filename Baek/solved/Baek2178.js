0261e1f673910e28b2b228218c657464e1a4474a
Spotky1004 <spotky1004@gmail.com>
2023-07-10 05:23:11 +0900

    Solved Baek 2178 (S1, 2023-07-10)

--- Baek/solved/Baek2178.js
+++ Baek/solved/Baek2178.js
@@ -1,128 +1,50 @@
-const rawInput = `4 4
-1111
-1111
-1111
-1111`;
-const input = rawInput.trim().split("\n").map(v => v.trim());
-
-const [sizeY, sizeX] = input[0].split(" ").map(v => +v);
-const maze = [...input.slice(1)].map(v => v.split("").map(v => +v));
+const isDev = process?.platform !== "linux";
+const input = (
+  !isDev
+    ? require("fs").readFileSync("/dev/stdin").toString()
+    :
+`2 25
+1011101110111011101110111
+1110111011101110111011101`
+)
+  .trim()
+  .split("\n");
 
 /**
- * @typedef Vector2
- * @property {number} x
- * @property {number} y
- */
-/**
- * @typedef SearchingData
- * @property {Vector2} at
- * @property {number} movedLength
- * @property {number[][]} map
- * @property {boolean} isDone
- */
-/** @type {SearchingData[]} */
-let searchingDatas = [
-  {
-    at: {x: 0, y: 0},
-    movedLength: 1,
-    map: [...maze.map(v => [...v])],
-    isDone: false
-  }
+ * @param {number} x 
+ * @param {number} y 
+*/
+function checkOOB(x, y) {
+  if (
+    0 > x || x >= M ||
+    0 > y || y >= N
+  ) return true;
+  return false;
+}
+
+
+
+const [N, M] = input.shift().split(" ").map(Number);
+const maze = input.map(v => Array.from(v).map(Number));
+
+const directions = [
+  [1, 0], [-1, 0],
+  [0, 1], [0, -1]
 ];
 
-const vectorsEnum = {
-  down: 0,
-  up: 1,
-  left: 2,
-  right: 3,
-}
-/** @type {Record<keyof typeof vectorsEnum, Vector2>} */
-const vectors = {
-  down: {x: 0, y: 1},
-  up: {x: 0, y: -1},
-  right: {x: 1, y: 0},
-  left: {x: -1, y: 0},
-}
+const queue = [[0, 0]];
+maze[0][0] = 2;
 
-/** @type {SearchingData[]} */
-let nextSearchingDatas = [];
-while (
-  searchingDatas.length !== 1 ||
-  searchingDatas[0].isDone === false
-) {
-  for (let i = 0; i < searchingDatas.length; i++) {
-    const sd = searchingDatas[i];
-    if (sd.isDone) {
-      let prevDoneIdx = nextSearchingDatas.findIndex(data => data.isDone);
-      if (
-        prevDoneIdx === -1 ||
-        nextSearchingDatas[prevDoneIdx].movedLength > sd.movedLength
-      ) {
-        nextSearchingDatas.splice(prevDoneIdx, 1);
-        nextSearchingDatas.push(sd);
-      }
-      continue;
-    }
+let count = 0;
+for (const [x, y] of queue) {
+  const curLen = maze[y][x];
 
-    const {x, y} = sd.at;
-    sd.map[y][x] = 0;
-    
-    const [
-      atBottomEdge,
-      atTopEdge,
-      atRightEdge,
-      atLeftEdge,
-    ] = [
-      y === sizeY-1,
-      y === 0,
-      x === sizeX-1,
-      x === 0
-    ];
-    const [
-      canGoDown,
-      canGoUp,
-      canGoRight,
-      canGoLeft,
-    ] = [
-      !atBottomEdge && sd.map[y+vectors.down.y][x+vectors.down.x] === 1,
-      !atTopEdge && sd.map[y+vectors.up.y][x+vectors.up.x] === 1,
-      !atRightEdge && sd.map[y+vectors.right.y][x+vectors.right.x] === 1,
-      !atLeftEdge && sd.map[y+vectors.left.y][x+vectors.left.x] === 1,
-    ];
-
-    const directions = ["down", "up", "right", "left"];
-    let canMoves = [canGoDown, canGoUp, canGoRight, canGoLeft];
-    if (canMoves.every(v => v === false)) continue;
-    for (let j = 0; j < directions.length; j++) {
-      if (!canMoves[j]) continue;
-
-      const direction = directions[j];
-      /** @type {Vector2} */
-      const moveVector = vectors[direction];
-      /** @type {SearchingData} */
-      const nextData = {
-        at: {
-          x: x+moveVector.x,
-          y: y+moveVector.y
-        },
-        map: [...sd.map.map(v => [...v])],
-        movedLength: sd.movedLength+1,
-        isDone: x+moveVector.x === sizeX-1 && y+moveVector.y === sizeY-1
-      };
-
-      const samePositionIndex = nextSearchingDatas.findIndex(data => data.at.x === nextData.at.x && data.at.y === nextData.at.y);
-      if (samePositionIndex !== -1) {
-        const canReplace = nextData.movedLength < nextSearchingDatas[samePositionIndex].movedLength;
-        if (canReplace) {
-          nextSearchingDatas[samePositionIndex] = nextData;
-        }
-      } else {
-        nextSearchingDatas.push(nextData);
-      }
-    }
+  for (const [dx, dy] of directions) {
+    const [tx, ty] = [x + dx, y + dy];
+    if (checkOOB(tx, ty) || maze[ty][tx] !== 1) continue;
+    maze[ty][tx] = curLen + 1;
+    queue.push([tx, ty]);
   }
-  searchingDatas = [...nextSearchingDatas];
-  nextSearchingDatas = [];
 }
 
-console.log(searchingDatas[0].movedLength);
+console.log(maze[N - 1][M - 1] - 1);

a6401efee87ee1af99863738399a040f5a1bebdb
Spotky1004 <spotky1004@gmail.com>
2023-07-28 00:29:11 +0900

    Update dijkstra module

--- Baek/module/graph/dijkstra.js
+++ Baek/module/graph/dijkstra.js
@@ -1,34 +1,18 @@
-// @ts-nocheck
-
-/** @type {Map<number, number>[]} */
-const nodesMap = Array(V + 1).fill().map(_ => new Map());
-for (const [from, to, cost] of rawLines) {
-  const nodeFrom = nodesMap[from];
-  nodeFrom.set(to, Math.min(nodeFrom.get(to) ?? Infinity, cost));
-}
-
-/**
- * @param {number} nodeCount 
- * @param {number} startNode 
- * @param {Map<number, number>[]} nodesMap 
- * @returns {number[]}
-*/
-function dijkstra(nodeCount, startNode, nodesMap) {
+function dijkstra(nodeCount, startNode) {
   const costs = Array(nodeCount + 1).fill(Infinity);
   costs[startNode] = 0;
-  /** @type {Heap<[node: number, cost: number]>} */
+  
   const heap = new Heap((a, b) => a[1] - b[1]);
-  heap.insert([startNode, 0]);
+  heap.push([startNode, 0]);
+
   while (heap.size > 0) {
-    /** @type {number} */
-    const [nodeNr, cost] = heap.delete();
+    const [nodeNr, cost] = heap.pop();
     if (costs[nodeNr] < cost) continue;
-    const node = nodesMap[nodeNr];
+    const node = connections[nodeNr];
     for (const [curNr, curCost] of node) {
-      if (costs[curNr] > cost + curCost) {
-        costs[curNr] = cost + curCost;
-        heap.insert([curNr, costs[curNr]]);
-      }
+      if (costs[curNr] < cost + curCost) continue;
+      costs[curNr] = cost + curCost;
+      heap.push([curNr, costs[curNr]]);
     }
   }
 

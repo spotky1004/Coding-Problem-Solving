77fbdb3c534cdb5068d8278fa6beabbffd5cbdb4
Spotky1004 <spotky1004@gmail.com>
2023-08-06 17:55:48 +0900

    Much better input

--- Baek/util/input.js
+++ Baek/util/input.js
@@ -1,10 +1,25 @@
-const isDev = process?.platform !== "linux";
-const [] = (
-  !isDev
-    ? require("fs").readFileSync("/dev/stdin").toString()
-    :
-``
-)
-  .trim()
-  .split("\n")
-  .map(line => line.split(" ").map(Number));
+const isDev = typeof window === "undefined" ?? require("os").userInfo().username === "spotky";
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  console.log(out);
+} else {
+  let CASE_NR = 0;
+  function check(input, answer) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const out = solve(input);
+    const deltaStr = (new Date().getTime() - startTime).toString();
+    const deltaZeroStr = " "+"0".repeat(6 - deltaStr.length);
+    if (out === answer) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[40m", `Case ${CASE_NR}: `, ` AC `, deltaZeroStr, deltaStr+"ms");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `Case ${CASE_NR}: `, ` WA `, deltaZeroStr, deltaStr+"ms\n", out);
+  }
+
+  check("", "");
+  check("", "");
+}
+
+function solve(input) {
+  return "";
+}

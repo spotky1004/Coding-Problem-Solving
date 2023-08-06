1b12c547a92afa85bd8253452ae58fd6545f8666
Spotky1004 <spotky1004@gmail.com>
2023-08-07 04:17:23 +0900

    Update input

--- Baek/util/input.js
+++ Baek/util/input.js
@@ -17,7 +17,7 @@ if (!isDev) {
     const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
     if (
       typeof answer === "string" ?
-        out.toString() === answer :
+        (out.toString() === answer || out.toString().trim() === answer) :
         answer.includes(out)
     ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `Case ${CASE_NR}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
     else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `Case ${CASE_NR}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out);

c12cbe483e18ce2bdbbdc2e9c125528fdae84788
Spotky1004 <spotky1004@gmail.com>
2023-08-08 01:00:31 +0900

    Update input

--- Baek/util/input.js
+++ Baek/util/input.js
@@ -7,7 +7,7 @@ if (!isDev) {
   console.log(out);
 } else {
   let CASE_NR = 0;
-  function check(input, answer, caseName=CASE_NR) {
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
     CASE_NR++;
     const startTime = new Date().getTime();
     const startMemory = !isWeb ? process.memoryUsage().heapUsed / 1024 : 0;
@@ -20,8 +20,8 @@ if (!isDev) {
       typeof answer === "string" ?
         out.toString() === answer :
         answer.includes(out)
-    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `Case ${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
-    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `Case ${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out);
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out);
   }
 
 // cases

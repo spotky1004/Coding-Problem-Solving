5a5e58069328cd59e6582d0cdaf98107bbe40955
Spotky1004 <spotky1004@gmail.com>
2023-09-01 13:47:48 +0900

    Fix input

--- Baek/util/input.js
+++ Baek/util/input.js
@@ -11,14 +11,14 @@ if (!isDev) {
     CASE_NR++;
     const startTime = new Date().getTime();
     const startMemory = !isWeb ? process.memoryUsage().heapUsed / 1024 : 0;
-    const out = solve(input);
+    const out = solve(input).toString();
     const timeDeltaStr = (new Date().getTime() - startTime).toString();
     const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
     const memoryDelta = ((!isWeb ? process.memoryUsage().heapUsed / 1024 : 0) - startMemory).toFixed(0);
     const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
     if (
       typeof answer === "string" ?
-        out.toString() === answer :
+        out === answer :
         answer.includes(out)
     ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
     else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));

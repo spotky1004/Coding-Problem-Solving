603d29903cd94f7331b4ab689d30d7105260277e
Spotky1004 <spotky1004@gmail.com>
2023-10-05 17:38:34 +0900

    Solved Baek 2777 (S2, 2023-10-05)

---
+++ Baek/solved/Baek2777.js
@@ -0,0 +1,73 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  console.log(out);
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed / 1024 : 0;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = ((!isWeb ? process.memoryUsage().heapUsed / 1024 : 0) - startMemory).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`2
+10
+26`,
+`2
+-1`);
+check(`2
+1
+2`,
+`1
+1`)
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [T, ...cases] = input
+  .trim()
+  .split("\n")
+  .map(Number);
+
+// code
+const out = [];
+for (const N of cases) {
+  const factors = Array(10).fill(0);
+  let n = N;
+  for (let i = 9; i >= 2; i--) {
+    while (n % i === 0) {
+      factors[i]++;
+      n /= i;
+    }
+  }
+
+  if (n !== 1) {
+    out.push(-1);
+    continue;
+  }
+  out.push(Math.max(1, factors.reduce((a, b) => a + b, 0)));
+}
+
+// output
+return out.join("\n");
+}

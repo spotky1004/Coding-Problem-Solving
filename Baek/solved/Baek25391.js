705a1780f8db9077dff80a806ea7fa5f013018ea
Spotky1004 <spotky1004@gmail.com>
2023-10-07 17:13:16 +0900

    Solved Baek 25391 (G5, 2023-10-07)

---
+++ Baek/solved/Baek25391.js
@@ -0,0 +1,66 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  console.log(out);
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed / 1024 : 0;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = ((!isWeb ? process.memoryUsage().heapUsed / 1024 : 0) - startMemory).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`7 2 3
+4 7
+7 8
+2 1
+9 3
+6 0
+10 4
+3 6`,
+`33`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [[N, M, K], ...scores] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+
+// code
+let sum = 0;
+
+scores.sort((a, b) => a[1] - b[1]);
+for (let i = 0; i < K; i++) {
+  sum += scores.pop()[0];
+}
+
+scores.sort((a, b) => a[0] - b[0]);
+for (let i = 0; i < M; i++) {
+  sum += scores.pop()[0];
+}
+
+// output
+return sum;
+}

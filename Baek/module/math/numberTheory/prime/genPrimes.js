ea733e2e68f980ee01cb521ae0f6d1921e69c2f8
Spotky1004 <spotky1004@gmail.com>
2023-10-08 19:05:50 +0900

    Update genPrimes module

--- Baek/module/math/numberTheory/prime/genPrimes.js
+++ Baek/module/math/numberTheory/prime/genPrimes.js
@@ -2,18 +2,17 @@
  * @param {number} n 
 */
 function genPrimes(n) {
-  /** @type {(number | null)[]} */
-  const net = Array.from({ length: n }, (_, i) => i);
-  net[0] = null;
-  net[1] = null;
-  for (let i = 4; i < net.length; i += 2) {
-    net[i] = null;
-  }
-  for (let i = 3; i < net.length; i++) {
-    if (net[i] === null) continue;
-    for (let j = i * 3; j < net.length; j += i * 2) {
-      net[j] = null;
+  /** @type {number[]} */
+  const primes = [];
+  const net = Array(n).fill(true);
+  for (let i = 2; i <= n; i++) {
+    if (net[i]) primes.push(i);
+    for (const p of primes) {
+      const a = i * p;
+      if (a > n) break;
+      net[a] = false;
+      if (i % p === 0) break;
     }
   }
-  return net.filter(v => v !== null);
+  return primes;
 }

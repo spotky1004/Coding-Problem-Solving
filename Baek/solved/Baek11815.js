c0f1e1527a684d657e07c58f563916d546f24606
Spotky1004 <spotky1004@gmail.com>
2023-10-21 17:12:37 +0900

    Solved Baek 11815 (S4, 2023-10-21)

---
+++ Baek/solved/Baek11815.js
@@ -0,0 +1,65 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  console.log(out);
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed / 1024 : 0;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = ((!isWeb ? process.memoryUsage().heapUsed / 1024 : 0) - startMemory).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`2
+4 5`,
+`1 0`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [[N], Xs] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(BigInt));
+
+// code
+function bigSqrt(x, m) {
+  let left = 0n, right = x * m;
+  while (left < right) {
+    const mid = (left + right) / 2n;
+    const midPow = mid ** 2n;
+    if (midPow > x) right = mid - 1n;
+    else if (midPow < x) left = mid + 1n;
+    else return mid;
+  }
+  return left;
+}
+
+const out = [];
+for (const X of Xs) {
+  out.push(bigSqrt(X, 1n)**2n === X ? 1 : 0);
+}
+
+// output
+return out.join(" ");
+}

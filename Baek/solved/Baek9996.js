1fddfe46f17c44553359033066e0dc66a54c82fc
Spotky1004 <spotky1004@gmail.com>
2023-10-21 16:48:15 +0900

    Solved Baek 9996 (S3, 2023-10-21)

---
+++ Baek/solved/Baek9996.js
@@ -0,0 +1,74 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  console.log(out);
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed / 1024 : 0;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = ((!isWeb ? process.memoryUsage().heapUsed / 1024 : 0) - startMemory).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`3
+a*d
+abcd
+anestonestod
+facebook`,
+`DA
+DA
+NE`);
+check(`6
+h*n
+huhovdjestvarnomozedocisvastan
+honijezakon
+atila
+je
+bio
+hun`,
+`DA
+DA
+NE
+NE
+NE
+DA`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+let [N, pattern, ...strs] = input
+  .trim()
+  .split("\n");
+N = Number(N);
+
+// code
+const regexpPattern = new RegExp(`^${pattern.replace(/\*/g, ".*")}$`);
+const out = [];
+for (const str of strs) {
+  if (str.match(regexpPattern)) out.push("DA");
+  else out.push("NE");
+}
+
+// output
+return out.join("\n");
+}

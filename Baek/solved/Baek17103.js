bf39247f473ac3995239dfbdecb2d51f7aa07f73
Spotky1004 <58690998+spotky1004@users.noreply.github.com>
2023-10-26 17:36:02 +0900

    Solved Baek 17103 (S2, 2023-10-26)

---
+++ Baek/solved/Baek17103.js
@@ -0,0 +1,117 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed / 1024 : 0;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = ((!isWeb ? process.memoryUsage().heapUsed / 1024 : 0) - startMemory).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`5
+6
+8
+10
+12
+100`,
+`1
+1
+2
+1
+6`);
+check(`10
+1000000
+1000000
+1000000
+1000000
+1000000
+1000000
+1000000
+1000000
+1000000
+1000000`,
+`5402
+5402
+5402
+5402
+5402
+5402
+5402
+5402
+5402
+5402`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [T, ...testCases] = input
+  .trim()
+  .split("\n")
+  .map(Number);
+
+// code
+/**
+ * @param {number} n 
+*/
+function genPrimes(n) {
+  /** @type {number[]} */
+  const primes = [];
+  const net = Array(n).fill(true);
+  for (let i = 2; i <= n; i++) {
+    if (net[i]) primes.push(i);
+    for (const p of primes) {
+      const a = i * p;
+      if (a > n) break;
+      net[a] = false;
+      if (i % p === 0) break;
+    }
+  }
+  return primes;
+}
+
+
+
+const primes = genPrimes(1e6);
+const isPrime = Array(1e6 + 1).fill(false);
+for (const p of primes) isPrime[p] = true;
+
+const out = [];
+for (const N of testCases) {
+  let count = 0;
+  for (const p of primes) {
+    if (p * 2 > N) break;
+    if (isPrime[N - p]) count++;
+  }
+  out.push(count);
+}
+
+// output
+return out.join("\n");
+}

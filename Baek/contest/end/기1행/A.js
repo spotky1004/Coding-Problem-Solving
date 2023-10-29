8161659e7600c37b7111e79d42ef720fd4ad4f18
Spotky1004 <spotky1004@gmail.com>
2023-10-30 08:50:00 +0900

    End contest "기1행"

---
+++ Baek/contest/end/기1행/A.js
@@ -0,0 +1,78 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed / 1024 : 0;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = ((!isWeb ? process.memoryUsage().heapUsed / 1024 : 0) - startMemory).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`5
+AABBA`,
+`2`);
+check(`5
+AAAAA`,
+`5`);
+check(`5
+ABABA`,
+`3`);
+check(`5
+AAABB`,
+`1`);
+check(`10
+AAAAAAAAAA`,
+`10`);
+check(`1
+A`,
+`1`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+let [N, song] = input
+  .trim()
+  .split("\n");
+N = Number(N);
+
+// code
+let curSong = song;
+let count = 0;
+for (let i = 0; i < N; i++) {
+  const sliced = curSong.slice(i);
+  if (song.startsWith(sliced)) {
+    count++;
+    curSong += song.slice(sliced.length);
+  }
+}
+
+// output
+return count;
+}

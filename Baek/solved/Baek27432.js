550565936b79eca4e30e5aaa527bafab0b49f5f1
Spotky1004 <spotky1004@gmail.com>
2023-11-22 01:55:35 +0900

    Solved Baek 27432 (first solve!, 2023-11-22)

---
+++ Baek/solved/Baek27432.js
@@ -0,0 +1,78 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed / 1024 : 0;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = ((!isWeb ? process.memoryUsage().heapUsed / 1024 : 0) - startMemory).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`2`, `0`);
+check(`3`, `1`);
+check(`4`, `11`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [[N]] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+
+// code
+const p = 999017;
+const m = Array(N).fill(0);
+m[0] = 0;
+m[1] = 1;
+m[2] = 1;
+for (let i = 3; i <= N; i++) {
+  m[i] = (2 * m[i - 1]) % p;
+}
+
+const dp = Array(N + 1).fill(0);
+for (let i = 1; i <= N; i++) {
+  dp[i] += m[i];
+  for (let j = 1; j < i; j++) {
+    dp[i] += (m[j] * dp[i - j]) % p;
+  }
+  dp[i] %= p;
+}
+
+let fact = 1;
+for (let i = 2; i <= N; i++) {
+  fact = (fact * i) % p;
+}
+
+const out = (fact - dp[N] + p) % p;
+
+// output
+return out;
+}

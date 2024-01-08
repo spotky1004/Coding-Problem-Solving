4b5080f0fa2a1655ef1c58696f6e2a92d40ea284
Spotky1004 <spotky1004@gmail.com>
2024-01-09 02:45:27 +0900

    End contest"제3회 흐즈로컵"

---
+++ Baek/contest/end/제3회 흐즈로컵/C.js
@@ -0,0 +1,74 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`4 6
+1 2
+1 3
+1 4
+2 3
+2 4
+3 4`,
+`4`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [[n, m], ...edges] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+
+// code
+function max(a, b) {
+  return a > b ? a : b;
+}
+
+const mod = 1_000_000_007n;
+const neighborCounts = Array(n).fill(0n);
+for (const [u, v] of edges) {
+  neighborCounts[u - 1]++;
+  neighborCounts[v - 1]++;
+}
+let sum = 0n;
+for (let i = 0; i < n; i++) {
+  const count = neighborCounts[i];
+  sum += max(0n, count * (count - 1n) * (count - 2n) / 6n);
+}
+
+// output
+return sum % mod;
+}

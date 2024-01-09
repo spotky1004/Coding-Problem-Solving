a1dcdde115909f3eb0d3b35e8d3ca6e34ce373d5
Spotky1004 <spotky1004@gmail.com>
2024-01-09 17:00:30 +0900

    Solved Baek 2294 (G5, 2024-01-09)

---
+++ Baek/solved/Baek2294.js
@@ -0,0 +1,67 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`3 15
+1
+5
+12`,
+`3`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+let [[n, k], ...coins] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+coins = coins.flat();
+
+// code
+const counts = [0];
+for (let i = 1; i <= k; i++) {
+  let count = Infinity;
+  for (let j = 0; j < n; j++) {
+    const coin = coins[j];
+    if (coin > i) continue;
+    count = Math.min(count, counts[i - coin] + 1);
+  }
+  counts[i] = count;
+}
+
+// output
+return isFinite(counts[k]) ? counts[k] : -1;
+}

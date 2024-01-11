8cb4cf17c47282a8b7df879ec2327c69f24b664f
Spotky1004 <spotky1004@gmail.com>
2024-01-12 08:39:01 +0900

    Solved Baek 2084 (G2, 2024-01-12)

---
+++ Baek/solved/Baek2084.js
@@ -0,0 +1,144 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`4
+3 2 2 3`,
+`0 1 1 1
+1 0 0 1
+1 0 0 1
+1 1 1 0`);
+check(`4
+2 2 2 3`,
+`-1`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [[N], degs] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+
+// code
+class Heap {
+  tree = [null];
+  compFunc = (a, b) => a - b;
+
+  constructor(compFunc) {
+    if (compFunc) this.compFunc = compFunc;
+  }
+
+  get size() {
+    return this.tree.length - 1;
+  }
+
+  first() {
+    return this.tree[1];
+  }
+
+  push(v) {
+    const tree = this.tree;
+    const compFunc = this.compFunc;
+
+    let curIdx = tree.length;
+    tree.push(v);
+    while (curIdx !== 1) {
+      const parentIdx = Math.floor(curIdx / 2);
+      if (compFunc(v, tree[parentIdx]) >= 0) break;
+      this.swap(curIdx, parentIdx);
+      curIdx = parentIdx;
+    }
+  }
+
+  pop() {
+    const tree = this.tree;
+    const compFunc = this.compFunc;
+    if (tree.length === 1) return null;
+    
+    const popedNode = tree[1];
+    if (tree.length === 2) {
+      this.tree = [null];
+      return popedNode;
+    }
+    
+    const lastNode = tree.pop();
+    tree[1] = lastNode;
+    let curIdx = 1;
+    while (true) {
+      const childIdx = tree.length <= curIdx * 2 + 1 || compFunc(tree[curIdx * 2], tree[curIdx * 2 + 1]) <= 0 ? curIdx * 2 : curIdx * 2 + 1;
+      if (
+        tree.length <= childIdx ||
+        compFunc(lastNode, tree[childIdx]) <= 0
+      ) break;
+      this.swap(curIdx, childIdx);
+      curIdx = childIdx;
+    }
+    
+    return popedNode;
+  }
+
+  swap(a, b) {
+    const tmp = this.tree[b];
+    this.tree[b] = this.tree[a];
+    this.tree[a] = tmp;
+  }
+}
+
+
+
+const out = Array.from({ length: N }, _ => Array(N).fill(0));
+
+const heap = new Heap((a, b) => b[1] - a[1]);
+for (let i = 0; i < N; i++) if (degs[i] > 0) heap.push([i, degs[i]]);
+
+while (heap.size > 0) {
+  const [u, uDeg] = heap.pop();
+
+  const tmp = [];
+  if (heap.size < uDeg) return -1;
+  for (let i = 0; i < uDeg; i++) {
+    const [v, vDeg] = heap.pop();
+    out[u][v] = 1;
+    out[v][u] = 1;
+    if (vDeg !== 1) tmp.push([v, vDeg - 1]);
+  }
+  while (tmp.length > 0) heap.push(tmp.pop());
+}
+
+// output
+return out.map(row => row.join(" ")).join("\n");
+}

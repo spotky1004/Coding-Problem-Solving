9069b6eb53b092f1235c76cff1db1ca4052eb2f6
Spotky1004 <spotky1004@gmail.com>
2024-01-12 11:08:40 +0900

    Solved Baek 17070 (G5, 2024-01-12)

---
+++ Baek/solved/Baek17070.js
@@ -0,0 +1,114 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`3
+0 0 0
+0 0 0
+0 0 0`,
+`1`);
+check(`4
+0 0 0 0
+0 0 0 0
+0 0 0 0
+0 0 0 0`,
+`3`);
+check(`5
+0 0 1 0 0
+0 0 0 0 0
+0 0 0 0 0
+0 0 0 0 0
+0 0 0 0 0`,
+`0`);
+check(`6
+0 0 0 0 0 0
+0 1 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0`,
+`13`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [[N], ...board] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+
+// code
+let count = 0;
+function isImpossible(x, y) {
+  return (
+    0 > x || x >= N ||
+    0 > y || y >= N ||
+    board[y][x] === 1
+  );
+}
+function search(x = 1, y = 0, dir = 0) {
+  if (
+    isImpossible(x, y) ||
+    (
+      dir === 2 &&
+      (
+        isImpossible(x - 1, y) ||
+        isImpossible(x, y - 1)
+      )
+    )
+  ) return;
+  if (x === N - 1 && y === N - 1) {
+    count++;
+    return;
+  }
+
+  if (dir === 0) {
+    search(x + 1, y, 0);
+    search(x + 1, y + 1, 2);
+  } else if (dir === 1) {
+    search(x, y + 1, 1);
+    search(x + 1, y + 1, 2);
+  } else if (dir === 2) {
+    search(x + 1, y, 0);
+    search(x, y + 1, 1);
+    search(x + 1, y + 1, 2);
+  }
+}
+search();
+
+// output
+return count;
+}

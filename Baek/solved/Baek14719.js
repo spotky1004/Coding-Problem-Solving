0ff42f8d9c720fe453a9a60af073b1e5d782b703
Spotky1004 <spotky1004@gmail.com>
2024-01-12 14:15:18 +0900

    Solved Baek 14719 (G5, 2024-01-12)

---
+++ Baek/solved/Baek14719.js
@@ -0,0 +1,74 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`4 4
+3 0 1 4`,
+`5`);
+check(`4 8
+3 1 2 3 4 1 1 2`,
+`5`);
+check(`3 5
+0 0 0 2 0`,
+`0`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [[H, W], heights] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+
+// code
+/** @type {[h: number, w: number][]} */
+const stack = [[Infinity, 0]];
+let maxHeight = 0;
+let waterBlocks = 0;
+for (const height of heights) {
+  let compressWidth = 1;
+  while (stack[stack.length - 1][0] <= height) {
+    const [h, w] = stack.pop();
+    if (stack.length !== 1) waterBlocks += (Math.min(maxHeight, height) - h) * w;
+    compressWidth += w;
+  }
+  stack.push([height, compressWidth]);
+  maxHeight = Math.max(maxHeight, height);
+}
+
+// output
+return waterBlocks;
+}

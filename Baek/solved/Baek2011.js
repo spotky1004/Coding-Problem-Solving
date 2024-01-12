643cfec1d3956d38ad20064ce28bac1d93280ba0
Spotky1004 <spotky1004@gmail.com>
2024-01-12 13:58:44 +0900

    Solved Baek 2011 (G5, 2024-01-12)

---
+++ Baek/solved/Baek2011.js
@@ -0,0 +1,67 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`25114`, `6`);
+check(`1111111111`, `89`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const code = input
+  .trim();
+
+// code
+if (
+  code[0] === "0" ||
+  code.includes("00")
+) return 0;
+
+const mod = 1_000_000;
+const dp = [1];
+for (let i = 1; i < code.length; i++) {
+  const twoDigits = Number(code[i - 1] + code[i]);
+  dp.push(
+    (
+      ((code[i] !== "0" ? dp[i - 1] : 0) ?? 0) + 
+      ((10 <= twoDigits && twoDigits <= 26 ? dp[i - 2] : 0) ?? 1)
+    ) % mod
+  );
+}
+
+// output
+return dp[code.length - 1];
+}

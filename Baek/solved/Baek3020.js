569fda7c47e42bed2e05d582f59effa6d5916f1c
Spotky1004 <spotky1004@gmail.com>
2024-01-13 10:51:01 +0900

    Solved Baek 3020 (G5, 2024-01-13)

---
+++ Baek/solved/Baek3020.js
@@ -0,0 +1,100 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`6 7
+1
+5
+3
+3
+5
+1`,
+`2 3`);
+check(`14 5
+1
+3
+4
+2
+2
+4
+3
+4
+3
+3
+3
+2
+3
+3`,
+`7 2`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+let [[N, H], ...stones] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+stones = stones.flat();
+
+// code
+const counts = Array(H + 1).fill(0);
+for (let i = 0; i < N; i++) {
+  const h = stones[i];
+  if (i % 2 === 0) {
+    counts[0]++;
+    counts[h]--;
+  } else {
+    counts[H - h]++;
+  }
+}
+counts.pop();
+
+let min = Infinity;
+let minCount = 0;
+let acc = 0;
+for (let i = 0; i < H; i++) {
+  acc += counts[i];
+  counts[i] = acc;
+  if (acc < min) {
+    min = acc;
+    minCount = 0;
+  }
+  if (min === acc) minCount++;
+}
+
+// output
+return `${min} ${minCount}`;
+}

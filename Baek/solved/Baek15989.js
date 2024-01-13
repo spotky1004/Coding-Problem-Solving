45f704ac9c180c1bb7f8b88b5c618fd69c6d9a5c
Spotky1004 <spotky1004@gmail.com>
2024-01-13 14:58:23 +0900

    Solved Baek 15989 (G5, 2024-01-13)

---
+++ Baek/solved/Baek15989.js
@@ -0,0 +1,74 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`3
+4
+7
+10`,
+`4
+8
+14`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [T, ...nums] = input
+  .trim()
+  .split("\n")
+  .map(Number);
+
+// code
+const dp = Array(10001).fill(1);
+for (let i = 10000; i >= 0; i--) {
+  for (let j = 2; j <= i; j += 2) {
+    dp[i] += dp[i - j];
+  }
+}
+for (let i = 10000; i >= 0; i--) {
+  for (let j = 3; j <= i; j += 3) {
+    dp[i] += dp[i - j];
+  }
+}
+
+const out = [];
+for (const num of nums) {
+  out.push(dp[num]);
+}
+
+// output
+return out.join("\n");
+}

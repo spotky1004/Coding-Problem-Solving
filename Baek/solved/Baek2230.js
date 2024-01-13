f9ff02d7662e7498505745a0b2527181ce783260
Spotky1004 <spotky1004@gmail.com>
2024-01-13 14:13:30 +0900

    Solved Baek 2230 (G5, 2024-01-13)

---
+++ Baek/solved/Baek2230.js
@@ -0,0 +1,72 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`3 3
+1
+5
+3`,
+`4`);
+check(`3 0
+1
+5
+3`,
+`0`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+let [[N, M], ...nums] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+nums = nums.flat();
+
+// code
+nums.sort((a, b) => a - b);
+let minDiff = Infinity;
+let r = N - 1;
+for (let l = 0; l < N; l++) {
+  while (nums[r] - nums[l] >= M) r--;
+  if (r < 0) r = 0;
+  while (nums[r] - nums[l] < M) r++;
+  if (r >= N) break;
+  minDiff = Math.min(minDiff, nums[r] - nums[l]);
+}
+
+// output
+return minDiff;
+}

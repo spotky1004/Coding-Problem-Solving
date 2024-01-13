e530fe6a2e62b48d55e1185f56eab300f8b5c918
Spotky1004 <spotky1004@gmail.com>
2024-01-13 14:49:40 +0900

    Solved Baek 2240 (G5, 2024-01-13)

---
+++ Baek/solved/Baek2240.js
@@ -0,0 +1,69 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`7 2
+2
+1
+1
+2
+2
+1
+1`,
+`6`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+let [[T, W], ...order] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+order = order.flat();
+
+// code
+const dp = Array.from({ length: W + 1 }, _ => Array(T).fill(0));
+for (let i = 0; i < T; i++) {
+  const tree = order[i];
+  for (let j = 0; j <= W; j++) {
+    const canCatch = (tree % 2) === ((j + 1) % 2);
+    dp[j][i] = Math.max((dp[j - 1] ?? [])[i] ?? 0, dp[j][i - 1] ?? 0) + canCatch;
+  }
+}
+
+// output
+return dp.flat().reduce((a, b) => Math.max(a, b), 0);
+}

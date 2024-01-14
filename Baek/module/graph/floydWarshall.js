cc82aa4126bf03aeb7dd9d19861212661c96f566
Spotky1004 <spotky1004@gmail.com>
2024-01-14 13:15:58 +0900

    Fix naming

--- Baek/module/graph/floydWarshall.js
+++ Baek/module/graph/floydWarshall.js
@@ -1,14 +1,14 @@
 /**
  * @param {number} V 
- * @param {[a: number, b: number, cost: number]} connections 
+ * @param {[a: number, b: number, cost: number]} edges 
  * @returns {number[][]} 
  */
-function floydWarshall(V, connections) {
+function floydWarshall(V, edges) {
   const costs = Array.from({ length: V }, _ => Array(V).fill(Infinity));
   for (let i = 0; i < V; i++) {
     costs[i][i] = 0;
   }
-  for (const [a, b, cost] of connections) {
+  for (const [a, b, cost] of edges) {
     costs[a - 1][b - 1] = cost;
     costs[b - 1][a - 1] = cost;
   }

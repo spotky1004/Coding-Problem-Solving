5cd2bfec5e13d86e55a605bf65683d2bc4b83839
Spotky1004 <spotky1004@gmail.com>
2024-01-15 14:14:39 +0900

    Solved Baek 7490 (G5, 2024-01-15)

---
+++ Baek/solved/Baek7490.js
@@ -0,0 +1,87 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`2
+3
+7`,
+`1+2-3
+
+1+2-3+4-5-6+7
+1+2-3-4+5+6-7
+1-2 3+4+5+6+7
+1-2 3-4 5+6 7
+1-2+3+4-5+6-7
+1-2-3-4-5+6+7`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [[T], ...cases] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+
+// code
+function search(n, i = 0, curExpr = "", zeroExprs = []) {
+  if (i === n) {
+    if (eval(curExpr.replace(/ /g, "")) === 0) {
+      zeroExprs.push(curExpr);
+    }
+    return;
+  }
+
+  curExpr += (i + 1);
+  if (i < n - 1) {
+    search(n, i + 1, curExpr + " ", zeroExprs);
+    search(n, i + 1, curExpr + "+", zeroExprs);
+    search(n, i + 1, curExpr + "-", zeroExprs);
+  } else {
+    search(n, i + 1, curExpr, zeroExprs);
+  }
+
+  return zeroExprs;
+}
+
+const out = [];
+for (const [N] of cases) {
+  const zeroExprs = search(N);
+  out.push(zeroExprs.join("\n") + "\n");
+}
+
+// output
+return out.join("\n");
+}

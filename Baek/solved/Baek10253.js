c200147e0f561a723e89e88f82106bc8e7ebcc6f
Spotky1004 <spotky1004@gmail.com>
2024-01-17 09:16:20 +0900

    Solved Baek 10253 (G5, 2024-01-17)

---
+++ Baek/solved/Baek10253.js
@@ -0,0 +1,86 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`3
+4 23
+5 7
+8 11`,
+`138
+70
+4070`);
+check(`1
+9999 10000`,
+`30371235615000`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [[T], ...fracs] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+
+// code
+/**
+ * @param {number} a 
+ * @param {number} b 
+*/
+function gcd(a, b) {
+  return b ? gcd(b, a%b) : a;
+}
+
+
+
+const out = [];
+for (let [a, b] of fracs) {
+  while (a >= 2) {
+    const x = Math.ceil(b / a);
+    const d = x / gcd(x, b) * b;
+    a = a * (d / b) - (d / x);
+    b = d;
+    
+    const div = gcd(a, b);
+    a /= div;
+    b /= div;
+  }
+
+  out.push(b);
+}
+
+// output
+return out.join("\n");
+}

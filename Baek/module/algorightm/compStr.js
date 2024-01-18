9e6719cde7f1e1817966aa32e1be13d2189ed091
Spotky1004 <spotky1004@gmail.com>
2024-01-18 16:34:39 +0900

    Rename strCompare -> compStr

--- Baek/module/algorightm/compStr.js
+++ Baek/module/algorightm/compStr.js
@@ -2,7 +2,7 @@
  * @param {string} a 
  * @param {string} b 
 */
-function strCompare(a, b) {
+function compStr(a, b) {
   const len = Math.min(a.length, b.length);
   for (let i = 0; i < len; i++) {
     if (a[i] === b[i]) continue;

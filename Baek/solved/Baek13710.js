ca2cbf5869f072bf29398484080890e926e25fc3
Spotky1004 <58690998+spotky1004@users.noreply.github.com>
2024-02-09 23:03:11 +0900

    Solved Baek 13710 (G1, 2024-02-09)

---
+++ Baek/solved/Baek13710.js
@@ -0,0 +1,73 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`2
+1 2`,
+`6`);
+check(`8
+0 1 0 1 1 0 0 1`,
+`20`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [[N], A] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(BigInt));
+
+// code
+let sum = 0n;
+const MAX = 1_000_000_000n;
+for (let b = 1n; b < MAX; b <<= 1n) {
+  const counts = [0n, 0n];
+  for (const Ai of A) {
+    const isOne = Ai & b;
+    if (isOne) {
+      const tmp = counts[0];
+      counts[0] = counts[1];
+      counts[1] = tmp + 1n;
+    } else {
+      counts[0]++;
+    }
+    sum += b * counts[1];
+  }
+}
+
+// output
+return sum;
+}

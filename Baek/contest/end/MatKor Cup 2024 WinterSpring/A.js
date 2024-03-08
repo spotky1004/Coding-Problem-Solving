57e944d35da535d975e14bbfe486f3c5a135af0e
Spotky1004 <58690998+spotky1004@users.noreply.github.com>
2024-03-09 08:09:46 +0900

    End contest "MatKor Cup 2024 WinterSpring"

---
+++ Baek/contest/end/MatKor Cup 2024 WinterSpring/A.js
@@ -0,0 +1,62 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`20 32`, `4048`);
+check(`4 4`, `2024`);
+check(`4 8`, `0`);
+check(`3 9`, `-1`);
+check(`2023 2024`, `1023132`);
+check(`2024 2023`, `-1`);
+check(`1000000 1000000`, `506000000`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [[X, Y]] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+
+// code
+const W = (2 * X - Y) / 4;
+if (W < 0 || X - Y > 0) {
+  return -1;
+}
+
+// output
+return Math.floor(2024 * W);
+}

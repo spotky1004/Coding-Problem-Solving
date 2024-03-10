8b934d8a1fea47570171d03fa3ea8c53f9f68478
Spotky1004 <58690998+spotky1004@users.noreply.github.com>
2024-03-10 10:00:36 +0900

    Solved Baek 26035 (G3, 2024-03-10)

---
+++ Baek/solved/Baek26035.js
@@ -0,0 +1,75 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`2 4`, `18`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [[N, M]] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(BigInt));
+
+// code
+/**
+ * @param {bigint} a 
+ * @param {bigint} b 
+ * @param {bigint} p
+*/
+function divAndPow(a, b, p) {
+  let out = 1n;
+  let curMul = a;
+  let bin = 1n;
+  while (bin <= b) {
+    if (b & bin) {
+      out = out*curMul % p;
+    }
+    bin *= 2n;
+    curMul = curMul**2n % p;
+  }
+  return out;
+}
+
+
+
+const mod = 1_000_000_007n;
+const ans = (divAndPow(2n, N, mod) + divAndPow(2n, M, mod) + mod - 2n) % mod;
+
+// output
+return ans.toString();
+}

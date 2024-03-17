fac710061ee38f8cd2c6a31c8c51fab263041930
Spotky1004 <58690998+spotky1004@users.noreply.github.com>
2024-03-17 13:41:19 +0900

    Solved Baek 27944 (P5, 2024-03-17)

---
+++ Baek/solved/Baek27944.js
@@ -0,0 +1,84 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`5
+2 6 8 9 5`,
+`3`);
+check(`5
+72 80 26 45 8`,
+`9`);
+check(`500000
+${"900 ".repeat(500000).trim()}`,
+`901`);
+check(`2
+1000000000001 500000000001`,
+`500000000001`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [[N], A] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+
+// code
+/**
+ * @param {number} a 
+ * @param {number} b 
+*/
+function gcd(a, b) {
+  return b ? gcd(b, a%b) : a;
+}
+
+
+
+let gcdVals = [A[0], A[0] + 1];
+for (const Ai of A) {
+  const newGcdVals = [];
+  for (const gcdVal of gcdVals) {
+    let newGcdVal = gcd(gcdVal, Ai);
+    if (newGcdVal > 1) newGcdVals.push(newGcdVal);
+    newGcdVal = gcd(gcdVal, Ai + 1);
+    if (newGcdVal > 1) newGcdVals.push(newGcdVal);
+  }
+  gcdVals = newGcdVals;
+}
+
+// output
+return Math.max(1, ...gcdVals);
+}

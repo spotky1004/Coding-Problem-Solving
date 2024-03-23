bffe4cacee70800e13f3e70d4f2c02923dd05b08
Spotky1004 <58690998+spotky1004@users.noreply.github.com>
2024-03-23 14:11:58 +0900

    Solved Baek 1273 (P3, 2024-03-23)

---
+++ Baek/solved/Baek1273.js
@@ -0,0 +1,313 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`6
+2 1 2 0 3 1
+1 2 2 2 2 0
+1 0 1 2 0 3
+2
+2 4`,
+`12
+7`);
+check(`10
+2 2 2 2 2 2 2 2 2 2
+3 3 3 3 3 3 3 3 3 3
+4 4 4 4 4 4 4 4 4 4
+6
+3 3 2 3 4 3`,
+`20
+20
+10
+50
+50
+50`)
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [[N], can5, can2, can1, [M], queries] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+
+// code
+/**
+ * @template T 
+ * @template U 
+ */
+class SegmentTree {
+  /**
+   * @typedef {T} TreeType 
+   * @typedef {TreeType extends readonly (infer ElementType)[] ? ElementType : never} ElementType 
+   * @typedef {(left: ElementType | U, right: ElementType | U) => ElementType} MergeFunc 
+   * @typedef {(element: ElementType | U, ...params: any[]) => ElementType} UpdateFunc 
+   * @typedef {UpdateFunc[]} UpdateFuncs 
+   */
+
+  /** @type {number} */
+  size = 0;
+  /** @type {number} */
+  height = 0;
+  /** @type {TreeType} */
+  tree = null;
+  /** @type {ElementType} */
+  defaultValue = null;
+  /** @type {MergeFunc} */
+  mergeFunc = null;
+  /** @type {UpdateFuncs} */
+  updateFuncs = [];
+
+  /**
+   * @typedef SegmentTreeOptions 
+   * @property {T} values 
+   * @property {U} defaultValue 
+   * @property {MergeFunc} mergeFunc 
+   * @property {UpdateFuncs} queryFuncs
+   */
+  /**
+   * @param {SegmentTreeOptions} options 
+   */
+  constructor(options) {
+    const { values, queryFuncs, mergeFunc, defaultValue } = options;
+    this.size = values.length;
+    this.height = Math.ceil(Math.log2(values.length)) + 1;
+    this.mergeFunc = mergeFunc;
+    this.updateFuncs = queryFuncs;
+
+    this.defaultValue = defaultValue;
+    this.tree = Array(1 << this.height).fill(this.defaultValue);
+
+    const leafStartIdx = 1 << (this.height - 1);
+    for (let i = 0; i < values.length; i++) {
+      this.tree[i + leafStartIdx] = values[i];
+    }
+
+    for (let depth = this.height - 1; depth >= 1; depth--) {
+      const startIdx = 1 << (depth - 1);
+      for (let i = startIdx; i < 2 * startIdx; i++) {
+        const leftEl = this.tree[i * 2];
+        const rightEl = this.tree[i * 2 + 1];
+
+        if (
+          leftEl === this.defaultValue &&
+          rightEl === this.defaultValue
+        ) continue;
+        this.tree[i] = this.mergeFunc(
+          leftEl !== this.defaultValue ? leftEl : defaultValue,
+          rightEl !== this.defaultValue ? rightEl : defaultValue
+        );
+      }
+    }
+  }
+
+  /**
+   * @param {number} type 
+   * @param {number} idx 
+   * @param  {...any} params 
+   */
+  update(type, idx, ...params) {
+    const realIdx = (1 << (this.height - 1)) + idx - 1;
+    this.tree[realIdx] = this.updateFuncs[type](
+      this.tree[realIdx] !== this.defaultValue ? this.tree[realIdx] : this.defaultValue,
+      ...params
+    );
+
+    for (let parentIdx = (realIdx / 2) | 0; parentIdx >= 1; parentIdx = (parentIdx / 2) | 0) {
+      const leftEl = this.tree[parentIdx * 2];
+      const rightEl = this.tree[parentIdx * 2 + 1];
+      this.tree[parentIdx] = this.mergeFunc(
+        leftEl !== this.defaultValue ? leftEl : this.defaultValue,
+        rightEl !== this.defaultValue ? rightEl : this.defaultValue,
+      );
+    }
+  }
+
+  /**
+   * @param {number} l 
+   * @param {number} r 
+   * @returns {ElementType} 
+   */
+  sum(l, r) {
+    if (l > r) [l, r] = [r, l];
+    return this.#sum(1, 1, 1 << (this.height - 1), l, r);
+  }
+
+  /**
+   * @param {number} idx 
+   * @param {number} s 
+   * @param {number} e 
+   * @param {number} l 
+   * @param {number} r 
+   * @returns {ElementType} 
+   */
+  #sum(idx, s, e, l, r) {
+    if (r < s || e < l) return this.defaultValue;
+    if (l <= s && e <= r) return this.tree[idx];
+
+    const m = ((s + e) / 2) | 0;
+    const lSum = this.#sum(idx * 2, s, m, l, r);
+    const rSum = this.#sum(idx * 2 + 1, m + 1, e, l, r);
+    return this.mergeFunc(
+      lSum !== this.defaultValue ? lSum : this.defaultValue,
+      rSum !== this.defaultValue ? rSum : this.defaultValue
+    );
+  }
+}
+
+/**
+ * @template T
+ */
+class Heap {
+  /** @type {T[]} */
+  tree = [null];
+  compFunc = (a, b) => a - b;
+
+  constructor(compFunc) {
+    if (compFunc) this.compFunc = compFunc;
+  }
+
+  get size() {
+    return this.tree.length - 1;
+  }
+
+  first() {
+    return this.tree[1];
+  }
+
+  push(v) {
+    const tree = this.tree;
+    const compFunc = this.compFunc;
+
+    let curIdx = tree.length;
+    tree.push(v);
+    while (curIdx !== 1) {
+      const parentIdx = Math.floor(curIdx / 2);
+      if (compFunc(v, tree[parentIdx]) >= 0) break;
+      this.swap(curIdx, parentIdx);
+      curIdx = parentIdx;
+    }
+  }
+
+  pop() {
+    const tree = this.tree;
+    const compFunc = this.compFunc;
+    if (tree.length === 1) return null;
+    
+    const popedNode = tree[1];
+    if (tree.length === 2) {
+      this.tree = [null];
+      return popedNode;
+    }
+    
+    const lastNode = tree.pop();
+    tree[1] = lastNode;
+    let curIdx = 1;
+    while (true) {
+      const childIdx = tree.length <= curIdx * 2 + 1 || compFunc(tree[curIdx * 2], tree[curIdx * 2 + 1]) <= 0 ? curIdx * 2 : curIdx * 2 + 1;
+      if (
+        tree.length <= childIdx ||
+        compFunc(lastNode, tree[childIdx]) <= 0
+      ) break;
+      this.swap(curIdx, childIdx);
+      curIdx = childIdx;
+    }
+    
+    return popedNode;
+  }
+
+  swap(a, b) {
+    const tmp = this.tree[b];
+    this.tree[b] = this.tree[a];
+    this.tree[a] = tmp;
+  }
+}
+
+
+
+const scores = [null];
+const canIdxes = Array(N).fill(0);
+/** @type {Heap<[col: number, canAcc: number]>} */
+const colHeap = new Heap((a, b) => a[1] - b[1]);
+for (let col = 0; col < N; col++) colHeap.push([col, can5[col]]);
+let scoreAcc = N;
+for (let floor = 0; floor < 6e6; floor++) {
+  while (colHeap.size > 0 && colHeap.first()[1] <= floor) {
+    const [col, canAcc] = colHeap.pop();
+    const canIdx = canIdxes[col];
+
+    let newCanAcc = canAcc;
+    if (canIdx === 0) {
+      newCanAcc += can2[col];
+      scoreAcc += 1;
+    } else if (canIdx === 1) {
+      newCanAcc += can1[col];
+      scoreAcc += 3;
+    } else if (canIdx === 2) {
+      scoreAcc -= 5;
+      continue;
+    }
+    canIdxes[col]++;
+    colHeap.push([col, newCanAcc]);
+  }
+  scores.push(scoreAcc);
+}
+
+const countSegTree = new SegmentTree({
+  values: Array(3e6).fill(1),
+  defaultValue: 0,
+  mergeFunc: (l, r) => l + r,
+  queryFuncs: [
+    () => 0
+  ]
+});
+
+const out = [];
+for (const floor of queries) {
+  let l = 0, r = 3e6 + 1;
+  while (l + 1 < r) {
+    const m = Math.floor((l + r) / 2);
+    if (countSegTree.sum(1, m) < floor) l = m;
+    else r = m;
+  }
+  const originalFloor = r;
+  countSegTree.update(0, originalFloor);
+  out.push(scores[originalFloor]);
+}
+
+// output
+return out.join("\n");
+}

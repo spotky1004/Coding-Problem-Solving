edf7ec76130a5d234dfd6957bb0e89bf570a7abe
Spotky1004 <58690998+spotky1004@users.noreply.github.com>
2024-03-29 16:14:25 +0900

    Solved Baek 7806 (G3, 2024-03-29)

---
+++ Baek/solved/Baek7806.js
@@ -0,0 +1,87 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`3 10
+10 240
+12 364
+100 2351
+629 163547`,
+`2
+240
+28
+1
+67`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const cases = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+
+// code
+const out = [];
+for (const [n, k] of cases) {
+  let gcd = 1;
+  let kTmp = k;
+  let sqrtK = Math.ceil(Math.sqrt(k));
+  for (let i = 2; i <= sqrtK; i++) {
+    let kCount = 0;
+    while (kTmp % i === 0) {
+      kCount++;
+      kTmp /= i;
+    }
+
+    let nCount = 0;
+    for (let nTmp = n; nTmp > 0; ) {
+      nTmp = Math.floor(nTmp / i);
+      nCount += nTmp;
+    }
+
+    gcd *= i**Math.min(kCount, nCount);
+  }
+  if (kTmp <= n) {
+    gcd *= kTmp;
+  }
+
+  out.push(gcd);
+}
+
+// output
+return out.join("\n");
+}

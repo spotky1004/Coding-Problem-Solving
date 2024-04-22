3f7a36ff9c63d234b2dbefee83e926d2d2597e70
Spotky1004 <58690998+spotky1004@users.noreply.github.com>
2024-04-22 19:40:02 +0900

    Faster fft module

--- _module/math/discreteMathematics/fft.js
+++ _module/math/discreteMathematics/fft.js
@@ -1,25 +1,4 @@
 /**
- * @typedef {[r: number, e: number]} Complex 
- * @typedef {(x: Complex, y: Complex) => Complex} ComplexBinOperation 
-*/
-
-/** @type {(n: number) => Complex} */
-const calcNthRootOfUnity = (n) => [Math.cos(2 * Math.PI / n), Math.sin(2 * Math.PI / n)];
-/** @type {(x: Complex) => Complex} */
-const Complex = (x) => [x, 0];
-/** @type {ComplexBinOperation} */
-const cAdd = (x, y) => [x[0] + y[0], x[1] + y[1]];
-/** @type {ComplexBinOperation} */
-const cSub = (x, y) => [x[0] - y[0], x[1] - y[1]];
-/** @type {ComplexBinOperation} */
-const cMul = (x, y) => [x[0] * y[0] - x[1] * y[1], x[0] * y[1] + x[1] * y[0]];
-/** @type {(x: Complex) => Complex} */
-const cInv = (x) => {
-  const den = x[0]**2 + x[1]**2;
-  return [x[0] / den, -x[1] / den];
-}
-
-/**
  * @param {number[]} a 
  * @param {number[]} b 
 */
@@ -28,10 +7,12 @@ function convolution(a, b) {
   const aPadLen = N - a.length;
   const bPadLen = N - b.length;
   const outLen = a.length + b.length - 1;
-  a = a.map(Complex);
-  b = b.map(Complex);
-  for (let i = 0; i < aPadLen; i++) a.push([0, 0]);
-  for (let i = 0; i < bPadLen; i++) b.push([0, 0]);
+  const a_r = [...a];
+  const b_r = [...b];
+  const a_i = Array(N).fill(0);
+  const b_i = Array(N).fill(0);
+  for (let i = 0; i < aPadLen; i++) a_r.push(0);
+  for (let i = 0; i < bPadLen; i++) b_r.push(0);
 
   const order = Array(N).fill(0);
   for (let i = 1; i < N; i <<= 1) {
@@ -41,64 +22,65 @@ function convolution(a, b) {
     }
   }
 
-  const w = calcNthRootOfUnity(N);
-  let wTmp = [1, 0];
-  const wPows = Array(N);
+  const unitRad = 2 * Math.PI / N;
+  const wp_rs = Array(N);
+  const wp_is = Array(N);
   for (let i = 0; i < N; i++) {
-    wPows[i] = wTmp;
-    wTmp = cMul(wTmp, w);
+    wp_rs[i] = Math.cos(i * unitRad);
+    wp_is[i] = Math.sin(i * unitRad);
   }
-  const wInv = cInv(calcNthRootOfUnity(N));
-  wTmp = [1, 0];
-  const wInvPows = Array(N);
+  const wip_is = Array(N);
+  for (let i = 0; i < N; i++) wip_is[i] = -wp_is[i];
+
+  dft(a_r, a_i, wp_rs, wp_is, order);
+  dft(b_r, b_i, wp_rs, wp_is, order);
   for (let i = 0; i < N; i++) {
-    wInvPows[i] = wTmp;
-    wTmp = cMul(wTmp, wInv);
+    [a_r[i], a_i[i]] = [a_r[i] * b_r[i] - a_i[i] * b_i[i], a_r[i] * b_i[i] + a_i[i] * b_r[i]];
   }
 
-  dft(a, wPows, order);
-  dft(b, wPows, order);
-  for (let i = 0; i < N; i++) {
-    a[i] = cMul(a[i], b[i]);
-  }
-
-  dft(a, wInvPows, order);
-  for (let i = 0; i < a.length; i++) {
-    a[i] = Math.round(a[i][0] / N);
-  }
-  while (a.length > outLen) a.pop();
+  dft(a_r, a_i, wp_rs, wip_is, order);
+  for (let i = 0; i < a_r.length; i++) a_r[i] = Math.round(a_r[i] / N);
+  while (a_r.length > outLen) a_r.pop();
   
-  return a;
+  return a_r;
 }
 
 /**
- * @param {number[]} f 
- * @param {Complex[]} w 
+ * @param {number[]} f_r 
+ * @param {number[]} f_i 
+ * @param {number[]} w_rs 
+ * @param {number[]} w_is 
  * @param {number[]} order 
 */
-function dft(f, wPows, order) {
-  const N = f.length;
+function dft(f_r, f_i, w_rs, w_is, order) {
+  const N = f_r.length;
 
-  let tmp;
   for (let i = 0; i < N; i++) {
     const x = order[i];
     const y = order[x];
     if (x >= y) continue;
-    tmp = f[x];
-    f[x] = f[y];
-    f[y] = tmp;
+    [f_r[x], f_r[y]] = [f_r[y], f_r[x]];
+    [f_i[x], f_i[y]] = [f_i[y], f_i[x]];
   }
 
+  let a_r, a_i, b_r, b_i, w_r, w_i, t_r, t_i, u_r, u_i;
   for (let i = 2; i <= N; i <<= 1) {
     const wPowAcc = N / i;
     const half = i / 2;
     for (let j = 0; j < N; j += i) {
       let wPow = 0;
       for (let k = 0; k < half; k++) {
-        const oMul = cMul(wPows[wPow], f[j + half + k]);
-        tmp = cSub(f[j + k], oMul);
-        f[j + k] = cAdd(f[j + k], oMul);
-        f[j + half + k] = tmp;
+        a_r = f_r[j + k], a_i = f_i[j + k];
+        b_r = f_r[j + k + half], b_i = f_i[j + k + half];
+        w_r = w_rs[wPow], w_i = w_is[wPow];
+        t_r = w_r * b_r - w_i * b_i, t_i = w_r * b_i + w_i * b_r;
+        [
+          f_r[j + k], f_i[j + k],
+          f_r[j + k + half], f_i[j + k + half]
+        ] = [
+          a_r + t_r, a_i + t_i,
+          a_r - t_r, a_i - t_i
+        ];
         wPow += wPowAcc;
       }
     }

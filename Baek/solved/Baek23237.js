cdf2171104a2f19b82ac6be3cc278fdd011ab482
Spotky1004 <58690998+spotky1004@users.noreply.github.com>
2024-04-23 15:48:56 +0900

    Solved Baek 23237 (G4, 2024-04-23)

---
+++ Baek/solved/Baek23237.js
@@ -0,0 +1,100 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`4
+0 1
+1 2
+2 3
+4
+0 1
+1 2
+1 3
+0`,
+`Case 1: 10
+Case 2: 11`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const lines = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+
+// code
+const out = [];
+let line = 0;
+let caseNr = 1;
+while (true) {
+  const [V] = lines[line++];
+  if (V === 0) break;
+  const adj = Array.from({ length: V }, () => []);
+  for (let i = 1; i < V; i++) {
+    const [A, B] = lines[line++];
+    adj[A].push(B);
+    adj[B].push(A);
+  }
+
+  let maxMask = (1 << V) - 1;
+  let count = 0;
+  for (let mask = 1; mask <= maxMask; mask++) {
+    const visited = Array(V).fill(false);
+    const subtree = [];
+    for (let b = 0; b < V; b++) {
+      if (mask & (1 << b)) subtree.push(b);
+    }
+    const isInSubtree = Array(V).fill(false);
+    for (const u of subtree) isInSubtree[u] = true;
+    const queue = [subtree[0]];
+    visited[queue[0]] = true;
+    for (const u of queue) {
+      for (const v of adj[u]) {
+        if (visited[v] || !isInSubtree[v]) continue;
+        visited[v] = true;
+        queue.push(v);
+      }
+    }
+    if (queue.length === subtree.length) count++;
+  }
+
+  out.push(`Case ${caseNr}: ${count}`);
+  caseNr++;
+}
+
+// output
+return out.join("\n");
+}

ad9679258250025ecf979e81aefc6256f771a88d
Spotky1004 <58690998+spotky1004@users.noreply.github.com>
2024-05-03 11:07:09 +0900

    Solved Baek 1522 (S1, 2024-05-03)

---
+++ Baek/solved/Baek1522.js
@@ -0,0 +1,75 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`abababababababa`, `3`);
+check(`ba`, `0`);
+check(`aaaabbbbba`, `0`);
+check(`abab`, `1`);
+check(`aabbaaabaaba`, `2`);
+check(`aaaa`, `0`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const S = input
+  .trim();
+
+// code
+let minCount = Infinity;
+for (let i = 0; i < S.length; i++) {
+  const Sp = S.slice(i) + S.slice(0, i);
+  let l = 0, r = S.length - 1;
+  let count = 0;
+  loop: while (l < r) {
+    if (Sp[l] === "a") {
+      l++;
+      continue;
+    }
+    while (Sp[r] !== "a") {
+      r--;
+      if (l >= r) break loop;
+    }
+    count++;
+    l++;
+    r--;
+  }
+  minCount = Math.min(minCount, count);
+}
+
+// output
+return minCount;
+}

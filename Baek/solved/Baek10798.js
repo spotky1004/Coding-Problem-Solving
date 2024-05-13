d5ee2a1a9e94cbf82177955d4bc1063a700e3547
Spotky1004 <58690998+spotky1004@users.noreply.github.com>
2024-05-13 14:46:29 +0900

    Solved Baek 10798 (B1, 2024-05-13)

---
+++ Baek/solved/Baek10798.js
@@ -0,0 +1,71 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`ABCDE
+abcde
+01234
+FGHIJ
+fghij`,
+`Aa0FfBb1GgCc2HhDd3IiEe4Jj`);
+check(`AABCDD
+afzz
+09121
+a8EWg6
+P5h3kx`,
+`Aa0aPAf985Bz1EhCz2W3D1gkD6x`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const lines = input
+  .trim()
+  .split("\n");
+
+// code
+const N = lines.length;
+const M = Math.max(...lines.map(v => v.length));
+let out = "";
+for (let i = 0; i < M; i++) {
+  for (let j = 0; j < N; j++) {
+    if (typeof lines[j][i] === "undefined") continue;
+    out += lines[j][i];
+  }
+}
+
+// output
+return out;
+}

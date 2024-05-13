2bd88e3b64d15dd2c65f472301a1119743924730
Spotky1004 <58690998+spotky1004@users.noreply.github.com>
2024-05-13 14:29:46 +0900

    Solved Baek 5622 (B2, 2024-05-13)

---
+++ Baek/solved/Baek5622.js
@@ -0,0 +1,66 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`WA`,
+`13`);
+check(`UNUCIC`,
+`36`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const S = input
+  .trim();
+
+// code
+const times = new Map([
+  ["A", 3], ["B", 3], ["C", 3],
+  ["D", 4], ["E", 4], ["F", 4],
+  ["G", 5], ["H", 5], ["I", 5],
+  ["J", 6], ["K", 6], ["L", 6],
+  ["M", 7], ["N", 7], ["O", 7],
+  ["P", 8], ["Q", 8], ["R", 8], ["S", 8],
+  ["T", 9], ["U", 9], ["V", 9],
+  ["W", 10], ["X", 10], ["Y", 10], ["Z", 10]
+]);
+
+let sum = 0;
+for (const Si of S) sum += times.get(Si);
+
+// output
+return sum;
+}

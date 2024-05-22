293ba32123ec115af39ed554d3b50d4106996a19
Spotky1004 <58690998+spotky1004@users.noreply.github.com>
2024-05-22 22:41:10 +0900

    Solved Baek 18230 (S1, 2024-05-22)

---
+++ Baek/solved/Baek18230.js
@@ -0,0 +1,77 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`5 4 3
+1 2 3 4
+4 5 6`,
+`15`);
+check(`12 3 5
+2 2 2
+1 3 8 7 3`,
+`26`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [[N, A, B], sticks, blocks] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+
+// code
+sticks.sort((a, b) => a - b);
+blocks.sort((a, b) => a - b);
+let space = N;
+let beauty = 0;
+if (space % 2 === 1) {
+  beauty += sticks.pop();
+  space--;
+}
+const stickMerged = [];
+while (sticks.length >= 2) stickMerged.push(sticks.pop() + sticks.pop());
+stickMerged.reverse();
+while (space > 0) {
+  const stickValue = stickMerged.length !== 0 ? stickMerged[stickMerged.length - 1] : 0;
+  const blockValue = blocks.length !== 0 ? blocks[blocks.length - 1] : 0;
+  if (blockValue > stickValue) beauty += blocks.pop();
+  else beauty += stickMerged.pop();
+  space -= 2;
+}
+
+// output
+return beauty;
+}

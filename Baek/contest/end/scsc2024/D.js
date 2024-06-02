eaefda46df0bf09b284c6c36f9e513f77be537ff
Spotky1004 <58690998+spotky1004@users.noreply.github.com>
2024-06-03 08:42:21 +0900

    Update D

--- Baek/contest/end/scsc2024/D.js
+++ Baek/contest/end/scsc2024/D.js
@@ -55,11 +55,146 @@ const [[T, N], [L, K], t, l] = input
   .map(line => line.split(" ").map(Number));
 
 // code
+/**
+ * @template T 
+ * @template U 
+ */
+class SegmentTree {
+  /**
+   * @typedef {T} TreeType 
+   * @typedef {TreeType extends readonly (infer ElementType)[] ? ElementType : never} ElementType 
+   * @typedef {(left: ElementType | U, right: ElementType | U) => ElementType} MergeFunc 
+   * @typedef {(element: ElementType | U, ...params: any[]) => ElementType} UpdateFunc 
+   * @typedef {UpdateFunc[]} UpdateFuncs 
+   */
+
+  /** @type {number} */
+  size = 0;
+  /** @type {number} */
+  height = 0;
+  /** @type {TreeType} */
+  tree = null;
+  /** @type {ElementType} */
+  defaultValue = null;
+  /** @type {MergeFunc} */
+  mergeFunc = null;
+  /** @type {UpdateFuncs} */
+  updateFuncs = [];
+
+  /**
+   * @typedef SegmentTreeOptions 
+   * @property {T} values 
+   * @property {U} defaultValue 
+   * @property {MergeFunc} mergeFunc 
+   * @property {UpdateFuncs} queryFuncs
+   */
+  /**
+   * @param {SegmentTreeOptions} options 
+   */
+  constructor(options) {
+    const { values, queryFuncs, mergeFunc, defaultValue } = options;
+    this.size = values.length;
+    this.height = Math.ceil(Math.log2(values.length)) + 1;
+    this.mergeFunc = mergeFunc;
+    this.updateFuncs = queryFuncs;
+
+    this.defaultValue = defaultValue;
+    this.tree = Array(1 << this.height).fill(this.defaultValue);
+
+    const leafStartIdx = 1 << (this.height - 1);
+    for (let i = 0; i < values.length; i++) {
+      this.tree[i + leafStartIdx] = values[i];
+    }
+
+    for (let depth = this.height - 1; depth >= 1; depth--) {
+      const startIdx = 1 << (depth - 1);
+      for (let i = startIdx; i < 2 * startIdx; i++) {
+        const leftEl = this.tree[i * 2];
+        const rightEl = this.tree[i * 2 + 1];
+
+        if (
+          leftEl === this.defaultValue &&
+          rightEl === this.defaultValue
+        ) continue;
+        this.tree[i] = this.mergeFunc(
+          leftEl !== this.defaultValue ? leftEl : defaultValue,
+          rightEl !== this.defaultValue ? rightEl : defaultValue
+        );
+      }
+    }
+  }
+
+  /**
+   * @param {number} type 
+   * @param {number} idx 
+   * @param  {...any} params 
+   */
+  update(type, idx, ...params) {
+    const realIdx = (1 << (this.height - 1)) + idx - 1;
+    this.tree[realIdx] = this.updateFuncs[type](
+      this.tree[realIdx] !== this.defaultValue ? this.tree[realIdx] : this.defaultValue,
+      ...params
+    );
+
+    for (let parentIdx = (realIdx / 2) | 0; parentIdx >= 1; parentIdx = (parentIdx / 2) | 0) {
+      const leftEl = this.tree[parentIdx * 2];
+      const rightEl = this.tree[parentIdx * 2 + 1];
+      this.tree[parentIdx] = this.mergeFunc(
+        leftEl !== this.defaultValue ? leftEl : this.defaultValue,
+        rightEl !== this.defaultValue ? rightEl : this.defaultValue,
+      );
+    }
+  }
+
+  /**
+   * @param {number} l 
+   * @param {number} r 
+   * @returns {ElementType} 
+   */
+  sum(l, r) {
+    if (l > r) [l, r] = [r, l];
+    return this.#sum(1, 1, 1 << (this.height - 1), l, r);
+  }
+
+  /**
+   * @param {number} idx 
+   * @param {number} s 
+   * @param {number} e 
+   * @param {number} l 
+   * @param {number} r 
+   * @returns {ElementType} 
+   */
+  #sum(idx, s, e, l, r) {
+    if (r < s || e < l) return this.defaultValue;
+    if (l <= s && e <= r) return this.tree[idx];
+
+    const m = ((s + e) / 2) | 0;
+    const lSum = this.#sum(idx * 2, s, m, l, r);
+    const rSum = this.#sum(idx * 2 + 1, m + 1, e, l, r);
+    return this.mergeFunc(
+      lSum !== this.defaultValue ? lSum : this.defaultValue,
+      rSum !== this.defaultValue ? rSum : this.defaultValue
+    );
+  }
+}
+
+
+
 let maxGap = 0, maxTerm = 0;
 for (let i = 1; i < K; i++) maxGap = Math.max(maxGap, l[i] - l[i - 1] - 1);
 for (let i = 1; i < N; i++) maxTerm = Math.max(maxTerm, t[i] - t[i - 1] - 1);
 maxTerm = Math.max(maxTerm, T - t[N - 1] - 1);
 if (maxGap > maxTerm) return "What is that map newbie...";
+
+const tDiff = t.slice(1).map((v, i) => v - t[i]);
+tDiff.push(t[N - 1]);
+
+const tDiffSegTree = new SegmentTree({
+  values: tDiff,
+  defaultValue: 0,
+  mergeFunc
+});
+
 let curT = 0;
 for (let i = 0; i < K - 1; i++) {
   const curGap = l[i + 1] - l[i] - 1;

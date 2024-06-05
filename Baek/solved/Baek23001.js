4db3b94b485d142886d9ed85f6b43991bbdffc07
Spotky1004 <58690998+spotky1004@users.noreply.github.com>
2024-06-05 10:06:45 +0900

    Solved Baek 23001 (G3, 2024-06-05)

---
+++ Baek/solved/Baek23001.js
@@ -0,0 +1,167 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`3
+1 3
+3 4 3
+1 3
+3 0 0
+3 3
+0 0 0
+0 2 0
+0 0 0`,
+`Case #1: 0
+Case #2: 3
+Case #3: 4`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [[T], ...lines] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+
+// code
+class Heap {
+  tree = [null];
+  compFunc = (a, b) => a - b;
+
+  constructor(compFunc) {
+    if (compFunc) this.compFunc = compFunc;
+  }
+
+  get size() {
+    return this.tree.length - 1;
+  }
+
+  first() {
+    return this.tree[1];
+  }
+
+  push(v) {
+    const tree = this.tree;
+    const compFunc = this.compFunc;
+
+    let curIdx = tree.length;
+    tree.push(v);
+    while (curIdx !== 1) {
+      const parentIdx = Math.floor(curIdx / 2);
+      if (compFunc(v, tree[parentIdx]) >= 0) break;
+      this.swap(curIdx, parentIdx);
+      curIdx = parentIdx;
+    }
+  }
+
+  pop() {
+    const tree = this.tree;
+    const compFunc = this.compFunc;
+    if (tree.length === 1) return null;
+    
+    const popedNode = tree[1];
+    if (tree.length === 2) {
+      this.tree = [null];
+      return popedNode;
+    }
+    
+    const lastNode = tree.pop();
+    tree[1] = lastNode;
+    let curIdx = 1;
+    while (true) {
+      const childIdx = tree.length <= curIdx * 2 + 1 || compFunc(tree[curIdx * 2], tree[curIdx * 2 + 1]) <= 0 ? curIdx * 2 : curIdx * 2 + 1;
+      if (
+        tree.length <= childIdx ||
+        compFunc(lastNode, tree[childIdx]) <= 0
+      ) break;
+      this.swap(curIdx, childIdx);
+      curIdx = childIdx;
+    }
+    
+    return popedNode;
+  }
+
+  swap(a, b) {
+    const tmp = this.tree[b];
+    this.tree[b] = this.tree[a];
+    this.tree[a] = tmp;
+  }
+}
+
+
+
+const directions = [
+  [1, 0], [-1, 0],
+  [0, 1], [0, -1]
+];
+
+const out = [];
+let line = 0;
+for (let tcNr = 0; tcNr < T; tcNr++) {
+  const [R, C] = lines[line++];
+  const board = lines.slice(line, line + R);
+  line += R;
+
+  const heap = new Heap((a, b) => b[2] - a[2]);
+  for (let r = 0; r < R; r++) {
+    for (let c = 0; c < C; c++) {
+      heap.push([r, c, board[r][c]]);
+    }
+  }
+  let ans = 0;
+  while (heap.size > 0) {
+    const [r, c, curCount] = heap.pop();
+    if (board[r][c] !== curCount) continue;
+    for (const [dr, dc] of directions) {
+      const [tr, tc] = [r + dr, c + dc];
+      if (
+        0 > tr || tr >= R ||
+        0 > tc || tc >= C
+      ) continue;
+      const nextCount = board[tr][tc];
+      const diff = curCount - nextCount;
+      if (diff <= 1) continue;
+      ans += diff - 1;
+      board[tr][tc] = curCount - 1;
+      heap.push([tr, tc, curCount - 1]);
+    }
+  }
+  out.push(`Case #${tcNr + 1}: ${ans}`);
+}
+
+// output
+return out.join("\n");
+}

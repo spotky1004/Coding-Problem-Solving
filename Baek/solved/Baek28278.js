8c2f8f4a6480e9011a80ee814742fc6dfee4d07d
Spotky1004 <58690998+spotky1004@users.noreply.github.com>
2024-06-06 20:08:24 +0900

    Solved Baek 28278 (S4, 2024-06-06)

---
+++ Baek/solved/Baek28278.js
@@ -0,0 +1,77 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`9
+4
+1 3
+1 5
+3
+2
+5
+2
+2
+5`,
+`1
+2
+5
+3
+3
+-1
+-1`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [[N], ...queries] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+
+// code
+const out = [];
+const stack = [];
+for (const [type, X] of queries) {
+  if (type === 1) stack.push(X);
+  else if (type === 2) out.push(stack.length > 0 ? stack.pop() : -1);
+  else if (type === 3) out.push(stack.length);
+  else if (type === 4) out.push(stack.length > 0 ? 0 : 1);
+  else if (type === 5) out.push(stack.length > 0 ? stack[stack.length - 1] : -1);
+}
+
+// output
+return out.join("\n");
+}

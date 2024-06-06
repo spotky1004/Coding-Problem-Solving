b065291ebde7b365617a03986817f9f5952159dd
Spotky1004 <58690998+spotky1004@users.noreply.github.com>
2024-06-06 17:31:13 +0900

    Solved Baek 5073 (B3, 2024-06-06)

---
+++ Baek/solved/Baek5073.js
@@ -0,0 +1,68 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`7 7 7
+6 5 4
+3 2 5
+6 2 6
+0 0 0`,
+`Equilateral
+Scalene
+Invalid
+Isosceles`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const cases = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+
+// code
+const out = [];
+for (const [a, b, c] of cases) {
+  if (a === 0 && b === 0 && c === 0) break;
+  if (a + b + c - 2 * Math.max(a, b, c) <= 0) out.push("Invalid");
+  else if (a === b && a === c) out.push("Equilateral");
+  else if (a === b || a === c || b === c) out.push("Isosceles");
+  else out.push("Scalene");
+}
+
+// output
+return out.join("\n");
+}

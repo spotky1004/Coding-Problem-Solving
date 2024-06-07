e7b5ef962faa64d55f1e34cb742757be03843f81
Spotky1004 <58690998+spotky1004@users.noreply.github.com>
2024-06-08 01:27:19 +0900

    Solved Baek 25912 (S4, 2024-06-08)

---
+++ Baek/solved/Baek25912.js
@@ -0,0 +1,81 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`9
+ENTER
+pjshwa
+chansol
+chogahui05
+lms0806
+pichulia
+r4pidstart
+swoon
+tony9402`,
+`8`);
+check(`7
+ENTER
+pjshwa
+chansol
+chogahui05
+ENTER
+pjshwa
+chansol`,
+`5`);
+check(`3
+ENTER
+lms0806
+lms0806`,
+`1`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [, ...S] = input
+  .trim()
+  .split("\n");
+
+// code
+let seen = new Set();
+let count = 0;
+for (const Si of S) {
+  if (Si === "ENTER") seen = new Set();
+  else count += !seen.has(Si), seen.add(Si);
+}
+
+// output
+return count;
+}

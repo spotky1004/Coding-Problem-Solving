618db266363c6b827708a35d158325e54e243ca8
Spotky1004 <58690998+spotky1004@users.noreply.github.com>
2024-06-08 02:00:50 +0900

    Solved Baek 4779 (S3, 2024-06-08)

---
+++ Baek/solved/Baek4779.js
@@ -0,0 +1,72 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`0
+1
+3
+2`,
+`-
+- -
+- -   - -         - -   - -
+- -   - -`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const cases = input
+  .trim()
+  .split("\n")
+  .map(Number);
+
+// code
+function search(N, out = []) {
+  if (N === 0) {
+    out.push("-");
+    return out;
+  }
+  search(N - 1, out);
+  out.push(" ".repeat(3 ** (N - 1)));
+  search(N - 1, out);
+  return out;
+}
+
+const out = [];
+for (const N of cases) out.push(search(N).join(""));
+
+// output
+return out.join("\n");
+}

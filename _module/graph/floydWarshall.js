6b0e738446d3f88a6fa1419099e885fd428babd7
Spotky1004 <58690998+spotky1004@users.noreply.github.com>
2024-06-10 15:16:36 +0900

    Update floydWarshall module

--- _module/graph/floydWarshall.js
+++ _module/graph/floydWarshall.js
@@ -2,24 +2,15 @@
  * @param {number} V 
  * @param {[a: number, b: number, cost: number]} edges 
  * @returns {number[][]} 
- */
+*/
 function floydWarshall(V, edges) {
   const costs = Array.from({ length: V }, _ => Array(V).fill(Infinity));
-  for (let i = 0; i < V; i++) {
-    costs[i][i] = 0;
-  }
-  for (const [a, b, cost] of edges) {
-    costs[a][b] = cost;
-    costs[b][a] = cost;
-  }
+  for (let i = 0; i < V; i++) costs[i][i] = 0;
+  for (const [u, v, cost] of edges) costs[u][v] = cost;
 
   for (let k = 0; k < V; k++) {
-    for (let i = 0; i < V; i++) {
-      for (let j = 0; j < V; j++) {
-        const newCost = Math.min(costs[i][j], costs[i][k] + costs[k][j]);
-        costs[i][j] = newCost;
-        costs[j][i] = newCost;
-      }
+    for (let u = 0; u < V; u++) {
+      for (let v = 0; v < V; v++) costs[u][v] = Math.min(costs[u][v], costs[u][k] + costs[k][v]);
     }
   }
   return costs;

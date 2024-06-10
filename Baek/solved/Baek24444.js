b8827fc9689f474b1834f5283f0c3a9c41704b15
Spotky1004 <58690998+spotky1004@users.noreply.github.com>
2024-06-10 14:46:13 +0900

    Solved Baek 24444 (S2, 2024-06-10)

---
+++ Baek/solved/Baek24444.js
@@ -0,0 +1,79 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`5 5 1
+1 4
+1 2
+2 3
+2 4
+3 4`,
+`1
+2
+4
+3
+0`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [[N, M, R], ...edges] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+
+// code
+const adj = Array.from({ length: N + 1 }, () => []);
+for (const [u, v] of edges) adj[u].push(v), adj[v].push(u);
+for (let i = 1; i <= N; i++) adj[i].sort((a, b) => a - b);
+
+const visited = Array(N + 1).fill(false);
+let idx = 1;
+const order = Array(N + 1).fill(0);
+const queue = [R];
+visited[R] = true;
+for (const u of queue) {
+  order[u] = idx++;
+  for (const v of adj[u]) {
+    if (visited[v]) continue;
+    visited[v] = true;
+    queue.push(v);
+  }
+}
+
+// output
+return order.slice(1).join("\n");
+}

bccee3119768882125a99480328402857577de39
Spotky1004 <58690998+spotky1004@users.noreply.github.com>
2024-06-10 15:07:51 +0900

    Solved Baek 1504 (G4, 2024-06-10)

---
+++ Baek/solved/Baek1504.js
@@ -0,0 +1,158 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`4 6
+1 2 3
+2 3 3
+3 4 1
+1 3 5
+2 4 5
+1 4 4
+2 3`,
+`7`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [[N, E], ...edges] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+const [v1, v2] = edges.pop();
+
+// code
+class Heap {
+  tree = [null];
+  compFunc = (a, b) => a - b;
+
+  constructor(compFunc) {
+    if (compFunc) this.compFunc = compFunc;
+  }
+
+  get size() {
+    return this.tree.length - 1;
+  }
+
+  first() {
+    return this.tree[1];
+  }
+
+  push(v) {
+    const tree = this.tree;
+    const compFunc = this.compFunc;
+
+    let curIdx = tree.length;
+    tree.push(v);
+    while (curIdx !== 1) {
+      const parentIdx = Math.floor(curIdx / 2);
+      if (compFunc(v, tree[parentIdx]) >= 0) break;
+      this.swap(curIdx, parentIdx);
+      curIdx = parentIdx;
+    }
+  }
+
+  pop() {
+    const tree = this.tree;
+    const compFunc = this.compFunc;
+    if (tree.length === 1) return null;
+    
+    const popedNode = tree[1];
+    if (tree.length === 2) {
+      this.tree = [null];
+      return popedNode;
+    }
+    
+    const lastNode = tree.pop();
+    tree[1] = lastNode;
+    let curIdx = 1;
+    while (true) {
+      const childIdx = tree.length <= curIdx * 2 + 1 || compFunc(tree[curIdx * 2], tree[curIdx * 2 + 1]) <= 0 ? curIdx * 2 : curIdx * 2 + 1;
+      if (
+        tree.length <= childIdx ||
+        compFunc(lastNode, tree[childIdx]) <= 0
+      ) break;
+      this.swap(curIdx, childIdx);
+      curIdx = childIdx;
+    }
+    
+    return popedNode;
+  }
+
+  swap(a, b) {
+    const tmp = this.tree[b];
+    this.tree[b] = this.tree[a];
+    this.tree[a] = tmp;
+  }
+}
+
+/**
+ * @param {number} V 
+ * @param {number} s 
+ * @param {[v: number, cost: number][]} adj 
+ */
+function dijkstra(V, s, adj) {
+  const costs = Array(V).fill(Infinity);
+  costs[s] = 0;
+  
+  const heap = new Heap((a, b) => a[1] - b[1]);
+  heap.push([s, 0]);
+
+  while (heap.size > 0) {
+    const [u, cost] = heap.pop();
+    if (costs[u] < cost) continue;
+    const node = adj[u];
+    for (const [v, costAdd] of node) {
+      if (costs[v] <= cost + costAdd) continue;
+      costs[v] = cost + costAdd;
+      heap.push([v, costs[v]]);
+    }
+  }
+
+  return costs;
+}
+
+
+
+const adj = Array.from({ length: N + 1 }, () => []);
+for (const [u, v, w] of edges) adj[u].push([v, w]), adj[v].push([u, w]);
+
+// output
+return isFinite(dijkstra(N + 1, 1, adj)[N]) ? Math.min(
+  dijkstra(N + 1, 1, adj)[v1] + dijkstra(N + 1, v1, adj)[v2] + dijkstra(N + 1, v2, adj)[N],
+  dijkstra(N + 1, 1, adj)[v2] + dijkstra(N + 1, v2, adj)[v1] + dijkstra(N + 1, v1, adj)[N]
+) : -1;
+}

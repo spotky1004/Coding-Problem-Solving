720c56f52d6baa7f3bb8200a8ca6fa8342837bc5
Spotky1004 <58690998+spotky1004@users.noreply.github.com>
2024-06-12 13:19:03 +0900

    Solved Baek 15703 (G4, 2024-06-12)

---
+++ Baek/solved/Baek15703.js
@@ -0,0 +1,83 @@
+const isWeb = typeof window === "object";
+const isDev = isWeb || require("fs").existsSync("C:/users/spotky");
+
+if (!isDev) {
+  const input = require("fs").readFileSync("/dev/stdin").toString();
+  const out = solve(input);
+  if (!isWeb) {
+    process.stdout.write(out.toString());
+    process.exit(0);
+  } else {
+    console.log(out);
+  }
+} else {
+  if (!isWeb) require('node:v8').setFlagsFromString('--stack-size=65536');
+
+  let CASE_NR = 1;
+  function check(input, answer, caseName=`Case ${CASE_NR}`) {
+    CASE_NR++;
+    const startTime = new Date().getTime();
+    const startMemory = !isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize;
+    const out = solve(input).toString().trim();
+    const timeDeltaStr = (new Date().getTime() - startTime).toString();
+    const timeDeltaZeroStr = " "+"0".repeat(6 - timeDeltaStr.length);
+    const memoryDelta = (((!isWeb ? process.memoryUsage().heapUsed : window.performance.memory.usedJSHeapSize) - startMemory) / 1024).toFixed(0);
+    const memoryDeltaZeroStr = " "+"0".repeat(8 - memoryDelta.length);
+    if (
+      typeof answer === "string" ?
+        out === answer :
+        answer.includes(out)
+    ) console.log("\x1b[1m%s\x1b[42m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[0m", `${caseName}: `, ` AC `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB");
+    else console.log("\x1b[1m%s\x1b[41m%s\x1b[0m\x1b[90m%s\x1b[0m%s\x1b[90m%s\x1b[0m%s\x1b[31m%s\x1b[0m", `${caseName}: `, ` WA `, timeDeltaZeroStr, timeDeltaStr+"ms", memoryDeltaZeroStr, memoryDelta+"KB\n", out.slice(0, 10000));
+  }
+
+// cases
+check(`4
+1 2 4 5`,
+`1`);
+check(`4
+1 2 1 2`,
+`2`);
+check(`5
+0 0 0 0 0`,
+`5`);
+check(`5
+3 3 3 2 2`,
+`2`);
+check(`5
+1 1 1 1 1`,
+`3`);
+check(`12
+0 0 1 1 2 2 3 3 4 4 5 5`,
+`2`);
+check(`5
+1 1 1 1 1`,
+`3`);
+}
+
+/**
+ * @param {string} input 
+ */
+function solve(input) {
+// input
+const [[N], A] = input
+  .trim()
+  .split("\n")
+  .map(line => line.split(" ").map(Number));
+
+// code
+A.sort((a, b) => a - b);
+let count = 0;
+while (A.length > 0) {
+  count++;
+  let lenLeft = A.pop();
+  while (A.length > 0 && lenLeft > 0) {
+    let i = A.length - 1;
+    while (A[i - 1] >= lenLeft - 1) i--;
+    lenLeft = Math.min(lenLeft - 1, A.splice(i, 1)[0]);
+  }
+}
+
+// output
+return count;
+}
